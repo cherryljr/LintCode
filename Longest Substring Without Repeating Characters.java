e713083a26d6fcd0f6c751751ef3ab68d6e0f2c8
cherryljr <travelerljr@gmail.com>
2017-06-22 00:40:41 +0900

    HashMap and Array

--- Longest Substring Without Repeating Characters.java
+++ Longest Substring Without Repeating Characters.java
@@ -1,5 +1,5 @@
 方法：用两个pointer, head和i.    
-   		HashMap<Char, Integer>: <character, last occurance index>
+   		HashMap<Char, Integer>
    		head从index 0 开始。若没有重复char, 每次只有for loop的i++。每次取substring[head,i]作为最新的string.
    		一旦有重复，那么意味着，从重复的老的那个index要往后加一格开始。所以head = map.get(i) +１.
 
@@ -57,5 +57,31 @@ public class Solution {
         }
         
         return max;
+    }
+}
+
+/********************************The other way********************************************************/
+
+public class Solution {
+    /**
+     * @param s: a string
+     * @return: an integer 
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int[] map = new int[256]; // map from character's ASCII to its last occured index
+        
+        int j = 0;
+        int i = 0;
+        int ans = 0;
+        for (i = 0; i < s.length(); i++) {
+            while (j < s.length() && map[s.charAt(j)]==0) {
+                map[s.charAt(j)] = 1;
+                ans = Math.max(ans, j-i + 1);
+                j ++;
+            }
+            map[s.charAt(i)] = 0;
+        }
+        
+        return ans;
     }
 }
