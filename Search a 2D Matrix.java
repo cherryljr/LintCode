50a7903da5c9ead15d2e99a0b6d85359d9fbcd5b
cherry <cherry>
2017-06-29 23:11:39 +0900

    BinarySearch Covert 2D int 1D

---
+++ Search a 2D Matrix.java
@@ -0,0 +1,126 @@
+/*
+Write an efficient algorithm that searches for a value in an m x n matrix.
+
+This matrix has the following properties:
+
+    * Integers in each row are sorted from left to right.
+
+    * The first integer of each row is greater than the last integer of the previous row.
+
+Example
+Consider the following matrix:
+
+[
+
+    [1, 3, 5, 7],
+
+    [10, 11, 16, 20],
+
+    [23, 30, 34, 50]
+
+]
+
+Given target = 3, return true.
+
+Challenge
+O(log(n) + log(m)) time
+log(m * n) = log(m) + log(n)
+
+Tags Expand 
+Binary Search Matrix
+*/
+
+
+
+// Binary Search Twice
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        if (matrix[0] == null || matrix[0].length == 0) {
+            return false;
+        }
+        
+        int row = matrix.length;
+        int column = matrix[0].length;
+        
+        // find the row index, the last number <= target 
+        int start = 0, end = row - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (matrix[mid][0] == target) {
+                return true;
+            } else if (matrix[mid][0] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (matrix[end][0] <= target) {
+            row = end;
+        } else if (matrix[start][0] <= target) {
+            row = start;
+        } else {
+            return false;
+        }
+        
+        // find the column index, the number equal to target
+        start = 0;
+        end = column - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (matrix[row][mid] == target) {
+                return true;
+            } else if (matrix[row][mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (matrix[row][start] == target) {
+            return true;
+        } else if (matrix[row][end] == target) {
+            return true;
+        }
+        return false;
+    }
+}
+
+//  We can try to do Binary Search Once
+//	Thoughts: Convert into a 1D array, BinarySearch
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        if (matrix[0] == null || matrix[0].length == 0) {
+            return false;
+        }
+        
+        int row = matrix.length, column = matrix[0].length;
+        int start = 0, end = row * column - 1;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            //	use mid / column to calculate the position in  row
+            // 	use mid % column to calculate the position in column
+            int number = matrix[mid / column][mid % column];
+            if (number == target) {
+                return true;
+            } else if (number < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (matrix[start / column][start % column] == target) {
+            return true;
+        } else if (matrix[end / column][end % column] == target) {
+            return true;
+        }
+        
+        return false;
+    }
+}
