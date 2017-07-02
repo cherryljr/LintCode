761dba0ed2102cf6a31cb94aa8d5e6d393b9d533
cherryljr <travelerljr@gmail.com>
2017-07-02 21:51:23 +0900

    Binary Search

---
+++ Search Insert Position.java
@@ -0,0 +1,91 @@
+一般的binary search.
+两种解法：
+	1. 找到第一个 >= target的数, return position.
+	2. 找到最后一个 < target的数， return position + 1.
+在理解上更为推荐第一种方法.
+在结尾判断该return 哪个position。(两种解法，先后问题)
+```
+/*
+28% Accepted
+Given a sorted array and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Example
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+
+Tags Expand 
+Binary Search Array Sorted Array
+*/
+
+// version 1: find the first position >= target
+public class Solution {
+    public int searchInsert(int[] A, int target) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int start = 0, end = A.length - 1;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                return mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (A[start] >= target) {
+            return start;
+        } else if (A[end] >= target) {
+            return end;
+        } else {
+            return end + 1;
+        }
+    }
+}
+
+// version 2: find the last position < target, return +1， 要特判一下target小于所有数组里面的元素
+
+public class Solution {
+    public int searchInsert(int[] A, int target) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int start = 0;
+        int end = A.length - 1;
+        int mid;
+        
+        if (target < A[0]) {
+            return 0;
+        }
+        // find the last number less than target
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                return mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (A[end] == target) {
+            return end;
+        }
+        if (A[end] < target) {
+            return end + 1;
+        }
+        if (A[start] == target) {
+            return start;
+        }
+        return start + 1;
+    }
+}
