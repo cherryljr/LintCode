55c369c3ffe5071034ef525fa32182f0e758ebc4
cherry <cherry>
2017-07-04 22:37:28 +0900

    Stack(no recursive) && Divide & Conquer

--- Binary Tree Postorder Traversal.java
+++ Binary Tree Postorder Traversal.java
@@ -30,6 +30,7 @@ Binary Tree
 
 */
 
+//	Divide & Conquer
 /*
 
 Thinking process:
@@ -61,6 +62,40 @@ public class Solution {
     }
 }
 
+
+// 1. Non-recursive, iterative
+public ArrayList<Integer> postorderTraversal(TreeNode root) {
+    ArrayList<Integer> result = new ArrayList<Integer>();
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    TreeNode prev = null; // previously traversed node
+    TreeNode curr = root;
+
+    if (root == null) {
+        return result;
+    }
+
+    stack.push(root);
+    while (!stack.empty()) {
+        curr = stack.peek();
+        if (prev == null || prev.left == curr || prev.right == curr) { // traverse down the tree
+            if (curr.left != null) {
+                stack.push(curr.left);
+            } else if (curr.right != null) {
+                stack.push(curr.right);
+            }
+        } else if (curr.left == prev) { // traverse up the tree from the left
+            if (curr.right != null) {
+                stack.push(curr.right);
+            }
+        } else { // traverse up the tree from the right
+            result.add(curr.val);
+            stack.pop();
+        }
+        prev = curr;
+    }
+
+    return result;
+}
 
 /*
     2. Non-recursive, iterative
