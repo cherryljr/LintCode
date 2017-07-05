fef7eb0773c4cc73d2b2ec97f80e36f275bb201e
cherryljr <travelerljr@gmail.com>
2017-07-05 13:06:52 +0900

    Stack(no recursive) like Binary Search Tree Iterator

--- Binary Tree Inorder Traversal.java
+++ Binary Tree Inorder Traversal.java
@@ -84,8 +84,12 @@ The code that how to Traversa the tree is very worth to learn. How smart way!!!
 */
 
 public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Inorder in ArrayList which contains node values.
+     */
     public ArrayList<Integer> inorderTraversal(TreeNode root) {
-        ArrayList<Integer> rst = new ArrayList<Integer>();
+         ArrayList<Integer> rst = new ArrayList<Integer>();
         if (root == null) {
             return rst;
         }
@@ -93,22 +97,15 @@ public class Solution {
         Stack<TreeNode> stack = new Stack<TreeNode>();
         TreeNode curr = root;
         
-        stack.push(curr);
-        while (!stack.isEmpty()) {
-            while (curr != null && curr.left != null) {
-                stack.push(curr.left);
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
                 curr = curr.left;
             }
-            //Pop the top node: the curr node
             curr = stack.pop();
             rst.add(curr.val);
-            //Move to right node, and push to stack if needed
-            curr = curr.right;
-            if (curr!= null) {
-                stack.push(curr);
-            }
-        }
-        
+            curr = curr.right;   
+        }   
         return rst;
-    }    
+    } 
 }
