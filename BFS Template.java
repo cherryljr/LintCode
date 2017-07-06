0b8e7f2334fe9fd103715c4e165156830cf85c49
cherryljr <travelerljr@gmail.com>
2017-07-06 16:17:20 +0900

    Use Queue Structure

--- BFS Template.java
+++ BFS Template.java
@@ -9,11 +9,14 @@ public class Solution {
         queue.offer(root);
         
         //	是否还有数需要遍历
+        //  每一次循环都是在遍历一层
         while (!queue.isEmpty()) {
+        	  //  level作为一个buffer储存遍历时每一层的元素
             ArrayList<Integer> level = new ArrayList<Integer>();
-            //	取出size后再for一遍，这是BFS的关键点所在
+            //	取出size后再for一遍，这是BFS的关键点所在。
+            //	循环中queue.size()是动态的，故必须在for循环之前就取好.
             int size = queue.size();
-            //	将下一层的数添加到队列中
+            //	利用for将该层每个点的孩子都添加到Queue中
             for (int i = 0; i < size; i++) {
                 TreeNode head = queue.poll();
                 level.add(head.val);
