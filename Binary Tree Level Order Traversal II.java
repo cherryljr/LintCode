a77af529e8a435dedb1ae281a204188636a643d4
cherryljr <travelerljr@gmail.com>
2017-07-06 17:13:18 +0900

    just reverse the result

---
+++ Binary Tree Level Order Traversal II.java
@@ -0,0 +1,76 @@
+Compared with BFS Template. Just reverse the result then achieve the goal.
+
+/*
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. 
+(ie, from left to right, level by level from leaf to root).
+
+Example
+Given binary tree {3,9,20,#,#,15,7},
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+ 
+
+return its bottom-up level order traversal as:
+
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+
+Tags Expand 
+Queue Binary Tree Binary Tree Traversal Breadth First Search
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: buttom-up level order a list of lists of integer
+     */
+    public ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root) {
+        // write your code here
+        ArrayList<ArrayList<Integer>> rst = new ArrayList();
+        
+        if (root == null) {
+            return rst;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            ArrayList<Integer> level = new ArrayList<Integer>();
+            for (int i = 0; i < size; i++)  {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            rst.add(level);
+        }
+        
+        Collections.reverse(rst);
+        return rst;
+    }
+}
