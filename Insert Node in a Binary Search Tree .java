a8502f673702c51ac933e87d8cb38d04b152552d
cherryljr <travelerljr@gmail.com>
2017-07-07 02:39:34 +0900

    Recursive & No Recursive

---
+++ Insert Node in a Binary Search Tree .java
@@ -0,0 +1,103 @@
+往Binary Search Tree里面加东西，一定会找到一个合适的leaf加上去。
+
+那么：就是说someNode.left or someNode.right是null时，就是insert node的地方。
+
+找到那个someNode就按照正常的Binary Search Tree规律。
+
+/*
+Given a binary search tree and a new tree node, insert the node into the tree. 
+You should keep the tree still be a valid binary search tree.
+
+Example
+Given binary search tree as follow, after Insert node 6, the tree should be:
+
+  2             2
+ / \           / \
+1   4   -->   1   4
+   /             / \ 
+  3             3   6
+Challenge
+Can you do it without recursion?
+
+Tags Expand 
+LintCode Copyright Binary Search Tree
+
+*/
+
+version 1: No Recursive
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of the binary search tree.
+     * @param node: insert this node into the binary search tree
+     * @return: The root of the new binary search tree.
+     */
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        if (root == null) {
+            root = node;
+            return root;
+        }
+        TreeNode tmp = root;
+        TreeNode last = null;
+        while (tmp != null) {
+            last = tmp;
+            if (tmp.val > node.val) {
+                tmp = tmp.left;
+            } else {
+                tmp = tmp.right;
+            }
+        }
+        if (last != null) {
+            if (last.val > node.val) {
+                last.left = node;
+            } else {
+                last.right = node;
+            }
+        }
+        return root;
+    }
+}
+
+
+version 2: Recursive
+二叉查找树中插入点的普遍做法——递归
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of the binary search tree.
+     * @param node: insert this node into the binary search tree
+     * @return: The root of the new binary search tree.
+     */
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        if (root == null) {
+            return node;
+        }
+        if (root.val > node.val) {
+            root.left = insertNode(root.left, node);
+        } else {
+            root.right = insertNode(root.right, node);
+        }
+        return root;
+    }
+}
