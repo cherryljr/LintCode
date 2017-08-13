1fbe637d5451216fde5077d7e3a1b04db056d7c2
cherryljr <travelerljr@gmail.com>
2017-08-13 14:33:30 +0900

    HashMap, index of Sum == 0

---
+++ Subarray Sum.java
@@ -0,0 +1,60 @@
+分析出，如果sum[0~a]=x, 然后sum[0~b]=x, 说明 a ~ b 之间数值没有发生变换
+因此可以得到 sum(a ~ b] = 0.    
+
+这样理解后，用hashMap存每个sum[0~i]的值和index i. 如果有重复，就找到了一组sum为0的数组。
+注意：存在a, b位置相等的情况，即 sum == 0 的情况为 0 ~ a / 0 ~ b. 如：[-1, 1]。这样会使得结果是空集。
+故我们需要对 HashMap 进行一次初始化，将 (0, -1) 推入表中。这样便可以解决该问题。
+
+/*
+Given an integer array, find a subarray where the sum of numbers is zero. 
+Your code should return the index of the first number and the index of the last number.
+
+Example
+Given [-3, 1, 2, -3, 4], return [0, 2] or [1, 3].
+
+Note
+There is at least one subarray that it's sum equals to zero.
+
+Tags Expand 
+Subarray Hash Table
+*/
+
+/*
+Thougths:
+Record the sum from (0 ~ a).
+Check sum on each index i, when found an existing sum in the hashMap, we are done.
+Reason:
+If adding all the numbers together, for example if sum[0 ~ a] = -3, ... sum[0 - b] = -3 again, a<b
+That means from a ~ b, there is not change: that is, sum[a - b] = 0.
+As result, hashmap.get(a)+1 will be the satrting index, and b will be ending index.
+*/
+
+public class Solution {
+
+    public ArrayList<Integer> subarraySum(int[] nums) {
+        ArrayList<Integer> rst = new ArrayList<Integer>();
+        if (nums == null || nums.length == 0) {
+            return rst;
+        }
+        
+        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        // Initialize
+        map.put(0, -1);
+        
+        //we know that sub-array (a,b) has zero sum if Sum(0 ... a-1) = Sum(0 ... b)
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            
+            if (map.containsKey(sum)) {
+                rst.add(map.get(sum) + 1);
+                rst.add(i);
+                return rst;
+            }
+            
+            map.put(sum, i);
+        }
+        
+        return rst;
+    }
+}
