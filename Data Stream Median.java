28cda9d1741b348f2990a8f57d6ec468fd492f20
cherryljr <travelerljr@gmail.com>
2017-08-16 17:55:12 +0900

    Heapsort minHeap maxHeap / PriorityQueue

--- Data Stream Median.java
+++ Data Stream Median.java
@@ -6,6 +6,8 @@
 
 Note:题目定义meadian = A[(n-1)/2],也就是说maxHeap需要和minHeap长度相等，或者多一个element,最后可以直接poll() and return.
 
+然而直接按照该想法编写程序会存在超时的情况。
+参考Blog,我们在原有的程序进行改进得到 Version 2 的程序,最终成功AC。
 /*
 Numbers keep coming, return the median of numbers at every time a new number added.
 
@@ -28,6 +30,7 @@ Tags Expand
 LintCode Copyright Heap Priority Queue
 */
 
+// Version 1
 public class Solution {
     /**
      * @param nums: A list of integers.
@@ -77,4 +80,57 @@ public class Solution {
     private int getMedian() {
         return maxHeap.peek();
     } 
+}
+
+// Version 2: Opitimized
+public class Solution {
+    /**
+     * @param nums: A list of integers.
+     * @return: the median of numbers
+     */
+    PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+    PriorityQueue<Integer> maxHeap = 
+        new PriorityQueue<Integer>(1, new Comparator<Integer>() {
+            public int compare(Integer left, Integer right) {
+                return right - left;
+            }    
+        });
+    private int numOfElements = 0;
+
+    public int[] medianII(int[] nums) {
+        int cnt = nums.length;
+        int[] ans = new int[cnt];
+        
+        for (int i = 0; i < cnt; ++i) {
+            addNumber(nums[i]);
+            ans[i] = getMedian();
+        }
+        
+        return ans;
+    }
+    
+    private void addNumber(int value) {
+        maxHeap.offer(value);
+        
+        if (numOfElements % 2 == 0) {
+            if (minHeap.isEmpty()) {
+                numOfElements++;
+                return;
+            }
+            else if (maxHeap.peek() > minHeap.peek()) {
+                Integer maxHeapRoot = maxHeap.poll();
+                Integer minHeapRoot = minHeap.poll();
+                maxHeap.offer(minHeapRoot);
+                minHeap.offer(maxHeapRoot);
+            }
+        }
+        else {
+            minHeap.offer(maxHeap.poll());
+        }
+        numOfElements++;
+    }
+    
+    private int getMedian() {
+        return maxHeap.peek();
+    }
 }
