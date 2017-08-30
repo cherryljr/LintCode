791030a97390f1a4725f8becb5c6ac93d32db1ba
cherryljr <travelerljr@gmail.com>
2017-08-31 01:29:16 +0900

    the same as Best Time to Buy and Sell Stock IV

--- Maximum Subarray III.java
+++ Maximum Subarray III.java
@@ -1,3 +1,14 @@
+典型划分类动态规划, 与 Best Time to Buy and Sell Stock IV 几乎相同
+
+State:
+	localMax[i][j] 表示前i个数，取j个子数组，包含第i个数的Maximum Sum
+	globalMax[i][j] 表示前i个数，取j个子数组，可以不包含第i个数的Maximum Sum
+Function:
+	localMax[i][j] = max(localMax[i - 1][j] + nums[i - 1], globalMax[i - 1][j - 1] + nums[i - 1])
+	globalMax[i][j] = max(globalMax[i - 1][j], localMax[i][j])
+Answer:
+	globalMax[nums.length][k]
+
 /*
 Description
 Given an array of integers and a number k, find k non-overlapping subarrays which have the largest sum.
@@ -14,37 +25,41 @@ Tags
 LintCode Copyright Subarray Array Dynamic Programming
 */
 
-public class Solution {
-    /**
-     * @param nums: A list of integers
-     * @param k: An integer denote to find k non-overlapping subarrays
-     * @return: An integer denote the sum of max k non-overlapping subarrays
-     */
-    public int maxSubArray(int[] nums, int k) {
-        if (nums.length < k) {
-            return 0;
-        }
-        int len = nums.length;
+public class Solution {  
+    /** 
+     * @param nums: A list of integers 
+     * @param k: An integer denote to find k non-overlapping subarrays 
+     * @return: An integer denote the sum of max k non-overlapping subarrays 
+     */  
+    public int maxSubArray(int[] nums, int k) {  
+        if(k > nums.length) {
+        	return 0;
+        }  
         
         // State
-        int[][] globalMax = new int[k + 1][len + 1];
-        int[][] localMax = new int[k + 1][len + 1];
+        int[][] local = new int[nums.length + 1][k + 1];  
+        int[][] global = new int[nums.length + 1][k + 1];  
         
-        // Initialize & Function
-        for (int i = 1; i <= k; i++) {
-            localMax[i][i-1] = Integer.MIN_VALUE;
-            // 小于 i 的数组不能够partition
-            for (int j = i; j <= len; j++) {
-                localMax[i][j] = Math.max(localMax[i][j-1], globalMax[i - 1][j-1]) + nums[j-1];
-                if (j == i)
-                    globalMax[i][j] = localMax[i][j];
-                else
-                    globalMax[i][j] = Math.max(globalMax[i][j-1], localMax[i][j]);
-            }
-        }
+        // if k=0 => local/global colomn 0 = 0  
+        // if nums.length=0 => local/global row 0 = 0 
+        // Initialize & Function 
+        for(int i = 1; i <= nums.length; i++) {  
+            local[i][0] = Integer.MIN_VALUE;  
+            for(int j = 1; j <= k; j++) {  
+                if(j > i) {	//矩阵中的值不能默认为0，否则影响结果  
+                    local[i][j] = Integer.MIN_VALUE;  
+                    global[i][j] = Integer.MIN_VALUE;  
+                    continue;  
+                }  
+                local[i][j] = Math.max(local[i - 1][j], global[i - 1][j - 1])  + nums[i - 1];  
+                if(i == j)  
+                    global[i][j] = local[i][j];  
+                else  
+                    global[i][j] = Math.max(global[i - 1][j], local[i][j]);  
+            }  
+        }  
         
-        // Answer
-        return globalMax[k][len];
-    }
-    
-}
+        // Answer  
+        return global[nums.length][k];  
+    }  
+}  
