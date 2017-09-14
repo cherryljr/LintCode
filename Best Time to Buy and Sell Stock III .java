b73dd83c8951738fed7c3178cf3dcf5f2c08f94c
cherryljr <travelerljr@gmail.com>
2017-09-14 17:01:12 +0900

    DP O(3N) & One for loop to O(N)

--- Best Time to Buy and Sell Stock III .java
+++ Best Time to Buy and Sell Stock III .java
@@ -13,21 +13,21 @@ Solution 2: Best Way
 	来自 LeetCode 上的一个优秀解法，在这里跟大家分享一下。
 	这里需要有四个变量 buy1, sell1, buy2, sell2.
 	int sell1 = 0, sell2 = 0, buy1 = Integer.MIN_VALUE, buy2 = Integer.MIN_VALUE;
-	
+
 	首先，刚刚开始可以看作我们身上是没有任何钱的，即 0.因此
 	buy1: 意味着我们需要从他人处借钱来购买Stock, 即 buy1 = 0 - prices[i]
 	这个时候我们希望借来的钱越少越好。故 buy1 = Math.max(buy1, -prices[i])
 	这里使用了 Math.max 运算，因为是借钱，所以这里的 buy1 是负数。
-	
+
 	sell1: 意味着我们想要卖出手里的Stock,所以我们可以收获到 prices[i] 的金钱，但是同时，我们需要还掉之前借来的钱 buy1.
 	即 sell1 = prices[i] - |buy1| = prices[i] + buy1			(注意 buy1 是负数)
 	同样的，我们希望赚到的钱尽量多。故 sell1 = Math.max(sell1, prices[i] + buy1)
-  
-  buy2: 意味着我们想要购买另外一只Stock,此时我们手里已经有了 sell1 的钱，因此购买之后我们手里还剩下 sell1 - prices[i].
-  同样的，我们希望花更少的钱买入，即留在手里的钱尽量的多。故 buy2 = Math.max(buy2, sell1 - prices[i])
-  
-  sell2: 意味着我们将卖掉第二只购买的Stock. 此时我们手里的钱为 buy2, 故卖出之后，我们能够拥有 buy2 + prices[i] 的钱
-  同样的，我们希望赚到的钱尽量多。故 sell2 = Math.max(sell2, buy2 + prices[i])
+
+	buy2: 意味着我们想要购买另外一只Stock,此时我们手里已经有了 sell1 的钱，因此购买之后我们手里还剩下 sell1 - prices[i].
+	同样的，我们希望花更少的钱买入，即留在手里的钱尽量的多。故 buy2 = Math.max(buy2, sell1 - prices[i])
+
+	sell2: 意味着我们将卖掉第二只购买的Stock. 此时我们手里的钱为 buy2, 故卖出之后，我们能够拥有 buy2 + prices[i] 的钱
+	同样的，我们希望赚到的钱尽量多。故 sell2 = Math.max(sell2, buy2 + prices[i])
   
 /*
 Describe:
