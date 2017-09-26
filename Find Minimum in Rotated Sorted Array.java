4d79d569f2622d2a302343035d3f6f6d99c9ab1f
cherryljr <travelerljr@gmail.com>
2017-09-26 16:42:33 +0900

    Binary Search

--- Find Minimum in Rotated Sorted Array.java
+++ Find Minimum in Rotated Sorted Array.java
@@ -22,6 +22,7 @@ Situation 2:
 if mid > end: that means there must be a mountain-jump somewhere after mid and before end, which is the minimum point. Now move start to mid.
 */
 
+// Solution 1
 public class Solution {
     /**
      * @param nums: a rotated sorted array
@@ -49,6 +50,33 @@ public class Solution {
         } else {
             return nums[end];
         }
+    }
+}
+
+// Solution 2
+public class Solution {
+    /**
+     * @param nums: a rotated sorted array
+     * @return: the minimum number in the array
+     */
+    public int findMin(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        
+        return nums[start];
     }
 }
 
