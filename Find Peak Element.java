6516fcd30716ab3c9a355fff90b2ebd618348efc
cherryljr <travelerljr@gmail.com>
2017-09-29 10:27:03 +0800

    Binary Search

--- Find Peak Element.java
+++ Find Peak Element.java
@@ -38,22 +38,24 @@ class Solution {
      * @return: return any of peek positions.
      */
     public int findPeak(int[] A) {
-        // write your code here
-        int start = 1, end = A.length-2; // 1.答案在之间，2.不会出界 
-        while(start + 1 <  end) {
+        int start = 1;
+        int end = A.length - 1; 
+        if (A[0] > A[1]) {
+		return 0;
+	}
+	if (A[end - 1] > A[end - 2]) {
+		return end - 1;
+	}
+        
+        while(start < end) {
             int mid = (start + end) / 2;
-            if(A[mid] < A[mid - 1]) {
-                end = mid;
-            } else if(A[mid] < A[mid + 1]) {
-                start = mid;
+            if(A[mid] < A[mid + 1]) {
+                start = mid + 1;
             } else {
                 end = mid;
             }
         }
-        if(A[start] < A[end]) {
-            return end;
-        } else { 
-            return start;
-        }
+        
+        return start;
     }
 }
