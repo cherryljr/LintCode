faeeed5b6e378261564f90846c9e3897c36ce5b6
cherryljr <travelerljr@gmail.com>
2017-10-04 14:43:42 +0900

    Two Pointers (Partition Array + findMid + Recursive) Using QuickSort Mind

--- Sort Colors II.java
+++ Sort Colors II.java
@@ -1,15 +1,15 @@
-������Ҫ����ķ�Χ�������� k,��ʱ�Ѿ�����ͨ�� ���� �㷨����һ���ˡ�
-�����޷�ͨ����һ�α������ܹ��õ������������ǿ���ʹ�������㷨��
-�������㷨��ʱ�临�Ӷ�����Ҳ��Ҫ O(nlogn),�������Ѿ�����������ķ�Χ 1~k.
-�����ǿ����ܹ�ͨ������Ϣ�����ӶȽ��͵� O(nlogk) �أ�
-���������뵽�� ���� ��˼�롣
-������������ Partition Array ��ͬ��ֻ�ǽ� Partition Array �е� k ÿ���滻�� colorMid ���ѡ�
-������
-	1. ����whileѭ����left ָ��������ұ�����ֱ�� left ָ��Ľڵ��ֵ���� colorMid.
-	2. ͬ�� right ָ��������������ֱ�� right ָ��Ľڵ��ֵС�� colorMid.
-	3. ����left��right�ڵ㣬ֱ��left��right�����ڵ����������ཻ��
-	4. �ݹ���ø÷������������� colorMid �� ��벿�� �� �Ұ벿�֡� ����ʵ����Ҳ�ǿ��ŵ�������
-	�㷨ʱ�临�Ӷ�Ϊ O(nlogk)
+该题需要排序的范围上升到了 k,此时已经和普通的 排序 算法几乎一样了。
+我们无法通过仅一次遍历便能够得到结果。因此我们考虑使用排序算法。
+而排序算法的时间复杂度最少也需要 O(nlogn),但该题已经给出了排序的范围 1~k.
+故我们考虑能够通过该信息将复杂度降低到 O(nlogk) 呢？
+于是我们想到了 二分 的思想。
+即处理方法与 Partition Array 相同，只是将 Partition Array 中的 k 每次替换成 colorMid 而已。
+做法：
+	1. 利用while循环，left 指针从左向右遍历，直到 left 指向的节点的值大于 colorMid.
+	2. 同理 right 指针从右向左遍历，直到 right 指向的节点的值小于 colorMid.
+	3. 交换left和right节点，直达left与right两个节点相遇或者相交。
+	4. 递归调用该方法，继续遍历 colorMid 的 左半部分 和 右半部分。 （即实质上也是快排的做法）
+	算法时间复杂度为 O(nlogk)
 	
 /*
 Description
@@ -76,7 +76,7 @@ class Solution {
                 l++;
                 r--;
             }
-        }
+        } 
         
         sortColors2Helper(colors, left, r, colorFrom, colorMid);
         sortColors2Helper(colors, l, right, colorMid + 1, colorTo);
