75873da9a5bc7217439b5aa5cd4f3f192e480e38
cherryljr <travelerljr@gmail.com>
2017-10-12 14:52:33 +0900

    QuickSelect & BFPRT (Top k question)

--- Kth Largest Element.java
+++ Kth Largest Element.java
@@ -6,12 +6,15 @@
 	O(NlogN) 时间复杂度 + O(1) 额外空间
 	2. 利用大小为 k 的堆来实现，可以降低时间复杂度
 	O(NlogK) 时间复杂度 + O(K) 额外空间
-	3. 利用到了 QuickSort 中的分支方法
-	O(N) 时间复杂度 / O(N^2) 最差情况下的时间复杂度 + O(1) 额外空间
-	4. 对第 3 种方法进行优化，随机化输入。使得 O(N) 的时间复杂度得以保证
+	3. 利用到了 QuickSort 中的分支方法 (Quickselect算法)
+	O(N) 时间复杂度 / O(N^2) 最差情况下的时间复杂度 + O(1) 额外空间 
+	4. 对第 3 种方法进行优化，随机化输入。使得 O(N) 的时间复杂度得以保证 (BFPRT算法)
+    该算法的一个典型应用便是 Top K 问题
 	O(N) 时间复杂度 + O(1) 额外空间
-具体代码解析详见：
-	https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/
+    关于 BFPRT算法，想要进一步了解的可以查看：http://www.jianshu.com/p/a43b0e1712d1
+    
+Get more details here：https://discuss.leetcode.com/topic/14597/solution-explained
+	
 
 /*
 Description
