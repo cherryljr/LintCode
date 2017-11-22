46d43b12fb01c85efb26bf0115d915328290fe3f
cherryljr <travelerljr@gmail.com>
2017-11-22 23:10:03 +0900

    Recursion & Stack & Morris (Threaded BinaryTree)

--- Binary Tree Inorder Traversal.java
+++ Binary Tree Inorder Traversal.java
@@ -2,15 +2,23 @@
 Recursive: Divide and Conquer, with helper(dfs) method
 
 法二:   
-Stack: 
+Using Stack: 
 Add left nodes all the way   
 Print curr   
 Move to right, add right if possible.   
   
-注意stack.pop()在加完left-most child 的后，一定要curr = curr.right.
-
+注意stack.pop()在add完 leftMost child 的后，一定要 curr = curr.right.
+即开始遍历其右子树。
 若不右移，很可能发生窘境：    
 curr下一轮还是去找自己的left-most child，不断重复curr and curr.left, 会infinite loop, 永远在左边上下上下。
+
+法三：
+Morris Traversal
+使用了线索二叉树这种新的数据结构来实现。
+https://en.wikipedia.org/wiki/Threaded_binary_tree
+https://leetcode.com/articles/binary-tree-inorder-traversal/
+
+二叉树的逆序中序遍历可见：Convert BST to Greater Tree (in leetcode)
 
 /*
 Given a binary tree, return the inorder traversal of its nodes' values.
@@ -32,11 +40,10 @@ Can you do it without recursion?
 
 Tags Expand 
 Recursion Binary Tree Binary Tree Traversal
-
 */
 
 /*
-    Recursive
+    Approach 1: Recursive
 */
 public class Solution {
     /**
@@ -64,12 +71,10 @@ public class Solution {
 }
 
 /*
-    2. Non-recursive
-   
+    Approach 2: Using Stack (Non-recursive)
 */
 
 /*
-
  Inorder traversal: use 1 stack, push left till end; pirnt/store curr; push right to stack
 
     Note: after curr = curr.right, curr could be null; this will skip the while loop, and move on to next element.
@@ -80,7 +85,6 @@ public class Solution {
     
 This method just like : "Binary Search Tree Iterator.java"
 The code that how to Traversa the tree is very worth to learn. How smart way!!!
-
 */
 
 public class Solution {
@@ -109,3 +113,78 @@ public class Solution {
         return rst;
     } 
 }
+
+/*
+    Approach 3: Morris Traversal
+*/
+/*
+There is a clever way to perform an in-order traversal using only linear time and constant space, 
+first described by J. H. Morris in his 1979 paper "Traversing Binary Trees Simply and Cheaply". 
+In this method, we have to use a new data structure-Threaded Binary Tree, and the strategy is as follows:
+	Step 1: Initialize current as root
+	Step 2: While current is not NULL,
+	If current does not have left child
+		a. Add current’s value
+		b. Go to the right, i.e., current = current.right
+	Else
+		a. In current's left subtree, make current the right child of the rightmost node
+		b. Go to this left child, i.e., current = current.left
+Complexity Analysis
+Time complexity : O(n). 
+	To prove that the time complexity is O(n), 
+	the biggest problem lies in finding the time complexity of finding the predecessor nodes of all the nodes in the binary tree. 
+	Intuitively, the complexity is O(nlogn), because to find the predecessor node for a single node related to the height of the tree. 
+	But in fact, finding the predecessor nodes for all nodes only needs O(n) time. 
+	Because a binary Tree with nn nodes has n−1 edges, the whole processing for each edges up to 2 times, 
+	one is to locate a node, and the other is to find the predecessor node. So the complexity is O(n).
+Space complexity : O(n). Arraylist of size nn is used.
+*/
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> rst = new ArrayList<>();
+        TreeNode curr = root;
+        
+        while (curr != null) {
+            /* 
+             * If there is no left subtree, then we can visit this node and
+             * continue traversing right.
+             */
+            if (curr.left == null) {
+                rst.add(curr.val);
+                // move to next right node
+                curr = curr.right;
+            } else {    // has a left subtree
+                TreeNode succ = getSuccessor(curr);  // get rightmost node
+                /* 
+                 * If the right subtree is null, then we have never been here before.
+                 * the current node should be the right child of succ.
+                 */
+                if (succ.right == null) {
+                    succ.right = curr;
+                    curr = curr.left;
+                } else {
+                /* 
+                 * If there is a right subtree, it is a link that we created on a
+                 * previous pass, so we should unlink it and visit this node to avoid infinite loops
+                 */
+                    succ.right = null;
+                    rst.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
+        
+        return rst;
+    }
+    
+    // Get the node with the largest value smaller than current node.
+    private TreeNode getSuccessor(TreeNode node) {
+        TreeNode succ = node.left;
+        while (succ.right != null && succ.right != node) {
+            succ = succ.right;
+        }
+        return succ;
+    }
+}
+
