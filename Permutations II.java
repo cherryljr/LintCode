a66f6ed3191383dea0ebe2d7801b534fee708c77
cherryljr <travelerljr@gmail.com>
2017-12-08 16:46:53 +0900

    Backtracking(DFS) & No Recursive

--- Permutations II.java
+++ Permutations II.java
@@ -1,21 +1,24 @@
-方法1:
-Mark visited. 并且要检查上一层recursive时有没有略过重复element. 因此需要排序，通过permutation规律查看是否排出了重复结果。
+方法1: 回溯法（DFS）
+    该方法与 Permutations 中的 Approach 1 类似。
+    Mark visited. 并且要检查上一层recursive时有没有略过重复element. 因此需要排序，通过permutation规律查看是否排出了重复结果。
 
-背景1:在recursive call里面有for loop, 每次从i=0开始, 试着在当下list上加上nums里面的每一个。    
-从i=0开始，所以会依次recursive每一个nums：因此，例如i=2,肯定比i=3先被访问。也就是:取i=2的那个list permutation肯定先排出来。   
+    背景1:
+    在recursive call里面有for loop, 每次从 i=0 开始, 试着在当下list上加上nums里面的每一个。    
+    从 i=0 开始，所以会依次recursive每一个nums, 因此，i=2,肯定比 i=3 先被访问。
+    也就是:取 i=2 的那个list permutation肯定先排出来。   
 
-背景2:重复的例子：给出Input[x, y1, y2], 假设y的值是一样的。那么，{x,y1,y2}和{x,y2,y1}是相同结果。
+    背景2:
+    重复的例子：给出Input[x, y1, y2], 假设y的值是一样的。那么，{x,y1,y2}和{x,y2,y1}是相同结果。
 
-综上，y1肯定比y2先被访问,{x,y1,y2}先出。 紧随其后，在另一个recursive循环里，{x,y2...}y2被先访问，跳过了y1。    
-重点:规律在此，如果跳过y1，也就是visited[y1] == false, 而num[y2] == num[y1]，那么这就是一个重复的结果，没必要做，越过。
+    综上，y1肯定比y2先被访问,{x,y1,y2}先出。 紧随其后，在另一个recursive循环里，{x,y2...}y2被先访问，跳过了y1。    
+    重点:规律在此，如果跳过y1，也就是visited[y1] == false, 而num[y2] == num[y1]，那么这就是一个重复的结果，没必要做，越过。
 
-结果:那么，我们需要input像{x,y1,y2}这样数值放一起，那么必须排序。
-
+结论:那么，我们需要input像{x,y1,y2}这样数值放一起，那么必须排序。
 
 方法2:
-一个办法就是给一个visited queue。 和queue在所有的地方一同populate. 然后visited里面存得时visited indexes。 (Not efficient code. check again)
+    一个办法就是给一个visited queue。 和queue在所有的地方一同populate. 然后visited里面存得时visited indexes。 
+    (Not efficient code. check again)
 
-```
 /*
 Given a list of numbers with duplicate number in it. Find all unique permutations.
 
