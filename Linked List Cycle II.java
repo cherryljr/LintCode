f7211969216058975220303941229e3b41d17e38
cherryljr <travelerljr@gmail.com>
2018-01-16 23:26:48 +0900

    HashMap & Slow/Fast Pointers O(1)

--- Linked List Cycle II.java
+++ Linked List Cycle II.java
@@ -7,9 +7,9 @@ O(1)的额外空间
 故，做法与Linked List Cycle相同。
 	1. 使用slow和fast进行移动，当slow == fast时跳出循环。表明链表中存在环。
 	2. 将slow移动回到head，fast保持不动.继续移动，但是此时移动方式方式变换，
-	二者均为一次仅移动一步.（程序中是令head进行移动，而slow从相遇点开始移动，效果是相同的）
+	二者均为一次仅移动一步.（程序中是令head进行移动，而slow(intersect)从相遇点开始移动，效果是相同的）
 	3. 当slow再次遇上fast时，该节点便是环的入口
-	（这里的证明与相关扩展可以参考：http://www.nowamagic.net/librarys/veda/detail/1842）
+	（这里的证明与相关扩展可以参考：http://www.cnblogs.com/hiddenfox/p/3408931.html）
 
 /*
 Given a linked list, return the node where the cycle begins.
@@ -41,8 +41,7 @@ Two Pointers Linked List
  * }
  */ 
 
-//	HashMap
-
+// Approach 1: HashMap
 public class Solution {
     public ListNode detectCycle(ListNode head) {  
         if (head == null) {
@@ -64,36 +63,61 @@ public class Solution {
 }
 
 
-//	Slow/Fast Pointer
-
+// Approach 2: Slow/Fast Pointer
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
 public class Solution {
     /**
      * @param head: The first node of linked list.
      * @return: The node where the cycle begins. 
      *           if there is no cycle, return null
      */
-    public ListNode detectCycle(ListNode head) {  
-        // write your code here
+    public ListNode detectCycle(ListNode head) {
         if (head == null || head.next == null) {
             return null;
         }
         
-        ListNode slow = head;
-        ListNode fast = head.next;
-        
-        while (slow != fast) {
-            if (fast == null || fast.next == null) {
-                return null;
-            }
-            slow = slow.next;
-            fast = fast.next.next;
+        // If there is a cycle, the fast/slow pointers will intersect at some node. 
+        // Otherwise, there is no cycle, so we cannot find an entrance to a cycle.
+        ListNode intersct = getIntersect(head);
+        if (intersct == null) {
+            return null;
         }
         
-        while (head != slow.next) {
-            slow = slow.next;
+        // To find the entrance to the cycle, we have two pointers traverse at
+        // the same speed -- one from the front of the list;
+        // and the other from the point of intersection.
+        while (head != intersct) {
             head = head.next;
+            intersct = intersct.next;
         }
-        
         return head;
     }
+    
+    // The same method as: "Linked List Cycle"
+    // Get the point where slow meet fast.
+    // If no cycle in the list, return null.
+    private ListNode getIntersect(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) { 
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return slow;
+            }
+        }
+        return null;
+    }
 }
+
