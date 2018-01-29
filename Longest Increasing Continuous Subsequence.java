62a3217671bb6b5c4119d3b9bc8a0243c858116c
cherryljr <travelerljr@gmail.com>
2018-01-29 20:51:03 +0900

    Forward-Backward Traversal & Sliding Window

--- Longest Increasing Continuous Subsequence.java
+++ Longest Increasing Continuous Subsequence.java
@@ -1,16 +1,3 @@
-该题有两种解法：
-方法一：
-	简单明了，分别 从左到右 和 从右到左 遍历一遍数组，如果发现下一个数比当前数要大则加一
-	否则置为1，最后返回 Math.max(length) 
-方法二：
-	Sequence DP
-	求最大值，不能调换元素位置 => Sequenc DP
-	与 Longest Increasing Subsequence 十分类似，区别在于本题要求为连续子序列。
-	并且有 从左到右 与 从右到左 两个方向。
-	故我们可以实现一个 DP 算法的方法来找到 LIS,
-	第一次调用得到 Left to Right 的 LIS, 然后调换数组顺序再次调用，得到 Right to Left 的 LIS
-	二者取最大值便是我们要的结果
-	
 /*
 Description
 Give an integer array，find the longest increasing continuous subsequence in this array.
@@ -32,18 +19,25 @@ Tags
 Array Dynamic Programming Enumeration
 */
 
-// Version 1
+/**
+ * Approach 1: Traverse Array Twice
+ * 简单明了，分别 从左到右 和 从右到左 遍历一遍数组.
+ * 分别求得两种方式遍历的出的 最长连续递增子序列。最后取较大值即可。
+ *  1. 对于 从左向右 遍历的方式，如果发现下一个数比当前数要大则 length + 1, 否则置为1.
+ *  2. 对于 从右向左 遍历的方式，如果发现上一个数比当前数要大则 length + 1, 否则置为1.
+ *  3. 取两次遍历得到的较大值即可.
+ */
 public class Solution {
     public int longestIncreasingContinuousSubsequence(int[] A) {
         if (A == null || A.length == 0) {
             return 0;
         }
-        
+
         int n = A.length;
         int answer = 1;
-        
+
         // from left to right
-        int length = 1; // just A[0] itself
+        int length = 1;
         for (int i = 1; i < n; i++) {
             if (A[i] > A[i - 1]) {
                 length++;
@@ -52,7 +46,7 @@ public class Solution {
             }
             answer = Math.max(answer, length);
         }
-        
+
         // from right to left
         length = 1;
         for (int i = n - 2; i >= 0; i--) {
@@ -63,61 +57,40 @@ public class Solution {
             }
             answer = Math.max(answer, length);
         }
-        
+
         return answer;
     }
 }
 
+/**
+ * Approach 2: Sliding Window
+ * 主要方法与 Approach 1 中的两次遍历相同。
+ * 但是在遍历的时候使用了 滑动窗口。
+ */
 public class Solution {
-    /**
-     * @param A an array of Integer
-     * @return  an integer
-     */
-    
-    int LIS(int[] A) {
-    	  // State
-        int[] f = new int[A.length];
-        
-        // Initialize & Function
-        int i, res = 0;
-        for (i = 0; i < A.length; ++i) {
-            f[i] = 1;
-            if (i > 0 && A[i-1] < A[i]) {
-                f[i] = f[i-1] + 1;
-            }
-            if (f[i] > res) {
-                res = f[i];
-            }
-        }
-        
-        // Answer
-        return res;
-    }
-     
     public int longestIncreasingContinuousSubsequence(int[] A) {
-        int n = A.length;
-        // Left to Rigth
-        int r1 = LIS(A);
-        int i = 0, j = n-1, t;
-        
-        // Change the Order
-        while (i < j) {
-            t = A[i];
-            A[i] = A[j];
-            A[j] = t;
-            ++i;
-            --j;
+        if (A == null || A.length == 0) {
+            return 0;
         }
-        
-        // Right to Left
-        int r2 = LIS(A);
-        
-        // Answer
-        if (r1 > r2) {
-            return r1;
+
+        int rst = 1, anchor = 0;
+        // from left to right
+        for (int i = 0; i < A.length; i++) {
+            if (i > 0 && A[i] <= A[i - 1]) {
+                anchor = i;
+            }
+            rst = Math.max(rst, i - anchor + 1);
         }
-        else {
-            return r2;
+
+        // from right to left
+        anchor = A.length - 1;
+        for (int i = A.length - 1; i >= 0; i--) {
+            if (i < A.length - 1 && A[i] <= A[i + 1]) {
+                anchor = i;
+            }
+            rst = Math.max(rst, anchor - i + 1);
         }
+
+        return rst;
     }
 }
