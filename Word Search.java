9f8a0fb6a1bc09a1fc1a017f7f560b19ad340220
cherryljr <travelerljr@gmail.com>
2018-02-12 00:28:29 +0900

    DFS (When should we do Backtracking)

--- Word Search.java
+++ Word Search.java
@@ -1,18 +1,5 @@
-Backtracking方法1:  基本依据Template的写法.
-用一个boolean visited[][] 来标记是否访问过。
-
-首先遍历整个二维字符数组，找到开头的字符后，进行 DFS : 每到一个字符，就将对应位置的 visited 置为 true.
-从一个字符出发，都可以朝四个方向走，只要有一个方向为 true 即可。当走的长度 index 与 word 长度相等时，
-则说明 word 能在字符表中被找到。
-Note: 记得回溯，即要将 visited 的值置回为 false.
-
-Backtracking方法2:
-在原来的基础上进行了改进，节约了 visited 数组的空间。
-方法:每次到一个字母，将其值置为特殊字符 ‘#’, 表示已经 check 过它.
-4个path recurse回来后，将board[i][j]的值恢复成原来的,即 Backtracking.
-总而言之就是这里 board 也充当了方法1中 visited 数组的作用。
-
 /*
+Description
 Given a 2D board and a word, find if the word exists in the grid.
 
 The word can be constructed from letters of sequentially adjacent cell, 
@@ -34,13 +21,37 @@ word = "ABCB", -> returns false.
 
 Tags Expand 
 Backtracking
-
 */
 
-//DFS: search through the board, going to different directions, while also increasing index. when index == word.length, that's end.
-//use visited[][] to mark visited places.
+/**
+ * Approach: DFS / Backtracking
+ * 利用一个数组 visited 来标记已经被遍历过的元素。
+ * 然后开始遍历整个二维字符数组，找到开头的字符后，进行 DFS :
+ * 每遍历到一个字符，就将对应位置的 visited 置为 true.
+ * 从一个字符出发，都可以朝四个方向走，只要有一个方向为 true 即可。
+ * 当走的长度 index 与 word 长度相等时，则说明 word 能在字符表中被找到。
+ *
+ * Note: 记得回溯，即要将 visited 的值置回为 false.
+ * 需要回溯的原因是：不能影响到以其他 路径 进行DFS时的情况。
+ * 当前visited只是标记以当前路径进行遍历时，遍历过的元素。
+ * 但是更换路径时，应该将已经遍历过的点的 visited 置回初始状态，以免对其他遍历过程产生影响。
+ * （形象点说就是：相当于顺着原来走过来的足迹走回去并且把它们擦掉）
+ * 本题中路径无法确定，并且每个点可能需要被多次遍历，所以需要进行 Backtracking, 类似的还有:
+ * Subset: https://github.com/cherryljr/LintCode/blob/master/Subset.java
+ * Permutations: https://github.com/cherryljr/LintCode/blob/master/Permutations%20II.java
+ * 这些题目中的元素都可能被多次遍历到，因此都使用了 Backtracking 这个方法。
+ * 而在 The Maze / Island 中，每个点总共只需要被遍历过一次即可（O(n))
+ * 在这种情况下，就是纯粹的 DFS 问题（当然也可以使用 BFS 解决）,
+ * 因为每个点只会被遍历一次，所以只要将被遍历过就将 visited[i][j] 置为 true 即可. 
+ * 不需要回溯。（不存在影响到其他遍历过程的问题）
+ *
+ * 为了节省空间，我们可以省去 visited 数组，而直接对原来的 board[][] 进行修改，
+ * 来区分是否被访问过。如:将被访问过的元素置为 '#'.
+ */
+class Solution {
+    // Marked the element that has been visited when DFS the board.
+    static boolean[][] visited;
 
-public class Solution {
     /**
      * @param board: A list of lists of character
      * @param word: A string
@@ -53,95 +64,41 @@ public class Solution {
         if (word == null || word.length() == 0) {
             return true;
         }
-        
-        int height = board.length; 
-        int width = board[0].length;
-        boolean[][] visited = new boolean[height][width];
-        
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    boolean rst = dfs(board, word, i, j, visited, 0);
-                    if (rst) {
-                        return true;
-                    }
+
+        visited = new boolean[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int  j = 0; j < board[0].length; j++) {
+                // if the start character of word is equals to the character board[i][j]
+                // we can do the dfs from here.
+                // And if the dfs's result it true, it means that we can find the word in board.
+                if ((word.charAt(0) == board[i][j]) && dfs(board, i, j, word, 0)) {
+                    return true;
                 }
             }
         }
-        
+
         return false;
     }
-    
-    private boolean dfs(char[][] board, String word, int i, int j, 
-                        boolean[][] visited, int index) {
-        int height = board.length;
-        int width = board[0].length;
+
+    private boolean dfs(char[][] board, int i, int j, String word, int index) {
         if (index == word.length()) {
             return true;
         }
-        if (i < 0 || i >= height || j < 0 || j >= width 
-            || board[i][j] != word.charAt(index) || visited[i][j]) {
-            return false;    
+        if (i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != word.charAt(index) || visited[i][j]) {
+            return false;
         }
-        
-        int[] move_i = {1, -1, 0, 0};
-        int[] move_j = {0, 0, 1, -1};
+
         visited[i][j] = true;
-        for (int k = 0; k < 4; k++) {
-            int next_i = i + move_i[k];
-            int next_j = j + move_j[k];
-            if (dfs(board, word, next_i, next_j, visited, index + 1)) {
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1 }};
+        for (int[] dir : dirs) {
+            // go to the four directions, check whether we can find the next character of the word or not.
+            if (dfs(board, i + dir[0], j + dir[1], word, index + 1)) {
                 return true;
             }
         }
         // Backtracking
         visited[i][j] = false;
-        
+
         return false;
     }
 }
-
-// Optimized, Save space N^2
-public class Solution {
-    public boolean exist(char[][] board, String word) {
-        if (word == null || word.length() == 0) {
-            return true;
-        }
-        if (board == null || board.length == 0) {
-            return false;
-        }
-        
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    boolean rst = search(board, word, i, j, 0);
-                    if (rst) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean search(char[][] board, String word, int i, int j, int start) {
-        if (start == word.length()) {
-            return true;
-        }
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(start)) {
-            return false;
-        }
-        
-        board[i][j] = '#';
-        boolean rst = search(board, word, i, j - 1, start + 1)
-        || search(board, word, i, j + 1, start + 1)
-        || search(board, word, i + 1, j, start + 1)
-        || search(board, word, i - 1, j, start + 1);   
-        // Backtracking
-        board[i][j] = word.charAt(start);
-        return rst;
-    }
-}
-
-
-
