6d8229b6e0e18eee0d43411769bba8a28b0312bb
cherryljr <travelerljr@gmail.com>
2018-02-17 18:53:23 +0900

    Two PriorityQueue (maxHeap + minHeap)

--- Data Stream Median.java
+++ Data Stream Median.java
@@ -1,13 +1,3 @@
-把Input stream想成向上的山坡。山坡中间那点，自然就是median.
-
-前半段，作为maxHeap,关注点是PriorityQueue的峰点，也就是实际上的median.   
-
-后半段，作为minHeap,正常的PriorityQueue。 开头是最小的。
-
-Note:题目定义meadian = A[(n-1)/2],也就是说maxHeap需要和minHeap长度相等，或者多一个element,最后可以直接poll() and return.
-
-然而直接按照该想法编写程序会存在超时的情况。
-参考Blog,我们在原有的程序进行改进得到 Version 2 的程序,最终成功AC。
 /*
 Numbers keep coming, return the median of numbers at every time a new number added.
 
@@ -30,59 +20,69 @@ Tags Expand
 LintCode Copyright Heap Priority Queue
 */
 
-// Version 1
+/**
+ * Approach 1: Two PriorityQueue (maxHeap + minHeap)
+ * 把 Input stream 想成向上的山坡。山坡中间那点，自然就是 median.
+ * 前半段，作为 maxHeap,关注点是 PriorityQueue 的峰点，也就是实际上的 median.
+ * 后半段，作为 minHeap,实现是Java中默认的 PriorityQueue。开头是最小的。
+ * 这边用到了一个小小的 trick:
+ *  maxHeap.size() 始终与 minHeap.size() 相等，或者更大 1.
+ *  这样我们需要的 median 元素就是 maxHeap.peek().
+ *  分析如下：
+ *  题目定义 median = A[(n-1)/2],也就是说：
+ *  当有 偶数 个元素时，此时 maxHeap.size() 与 minHeap.size() 相等，返回靠前的那个元素，即 median = maxHeap.peek()
+ *  当有 奇数 个元素时，此时 maxHeap.size() 比 minHeap.size() 大一，返回中间的那个元素，即 median = maxHeap.peek()
+ */
 public class Solution {
     /**
      * @param nums: A list of integers.
      * @return: the median of numbers
      */
-    PriorityQueue<Integer> maxHeap = 
-        new PriorityQueue<Integer>(1, new Comparator<Integer>() {
-            public int compare(Integer left, Integer right) {
-                return right - left;
-            }    
-        });
-    PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
-     
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b.compareTo(a));
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
     public int[] medianII(int[] nums) {
         int[] rst = new int[nums.length];
         if (nums == null || nums.length == 0) {
             return rst;
         }
-        
-        // Initialize
+
+        // Initialize the maxHeap
+        // Otherwise, it will throw Exception at addNumber Funtion when we visit maxHeap.peek()
         rst[0] = nums[0];
         maxHeap.offer(nums[0]);
-        
+
         // Function
         for (int i = 1; i < nums.length; i++) {
             addNumber(nums[i]);
             rst[i] = getMedian();
         }
-        
+
         return rst;
     }
-    
+
     private void addNumber(int value) {
         if (value > maxHeap.peek()) {
             minHeap.offer(value);
         } else {
             maxHeap.offer(value);
         }
-        
-        if (maxHeap.size() > minHeap.size() + 1) {
+
+        // Check the size of two heap is whether balanced or not
+        // if not equal then maxHeap will be larger by one
+        if (maxHeap.size() - minHeap.size() > 1) {
             minHeap.offer(maxHeap.poll());
         } else if (maxHeap.size() < minHeap.size()) {
             maxHeap.offer(minHeap.poll());
         }
     }
-    
+
     private int getMedian() {
         return maxHeap.peek();
-    } 
+    }
 }
 
-// Version 2: Opitimized
+// Approach 2: Optimize the add Function
 public class Solution {
     /**
      * @param nums: A list of integers.
