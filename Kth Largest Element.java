bee6278840eec7d88695e611736c34c026956e03
cherryljr <travelerljr@gmail.com>
2018-02-25 16:04:57 +0900

    QuickSelect | with Random Pivot

--- Kth Largest Element.java
+++ Kth Largest Element.java
@@ -17,15 +17,15 @@ Sort Quick Sort
 */
 
 /**
- * 用到的知识点：Quickselect, 详细介绍参见：
+ * 用到的知识点：QuickSelect, 详细介绍参见：
  * https://en.wikipedia.org/wiki/Quickselect
- * 
+ *
  * 解法的分析历程，一步步推进优化：
  *  1. 对整个数据进行 Sort, 然后取第 k 个元素即可。
- *  O(NlogN) 时间复杂度 + O(1) 额外空间
+ *  O(nlogn) 时间复杂度 + O(1) 额外空间
  *  2. 利用大小为 k 的堆来实现，可以降低时间复杂度
- *  O(NlogK) 时间复杂度 + O(K) 额外空间
- *  3. 利用到了 QuickSort 中的分支方法 (Quickselect算法)
+ *  O(nlogk) 时间复杂度 + O(K) 额外空间
+ *  3. 利用到了 QuickSort 中的 Partition 方法 (QuickSelect算法)
  *  O(N) 时间复杂度 / 最差情况下O(N^2)时间复杂度 + O(1) 额外空间
  *  时间复杂度分析：
  *      该算法实际上就是 利用了快速排序的 Partition 方法。
@@ -33,14 +33,14 @@ Sort Quick Sort
  *      因此时间复杂度为: n + n/2 + n/4 + n/8 + ... + 1 = 2n => O(n)
  *      而之所以会出现 O(n^2) 的最坏情况原因与 快速排序 相同。
  *      即当我们 pivot 选择不好，每次只能筛去一个元素，使得其退化成 n + n-1 + n-2 + ... + 1 = n(n+1)/2 = O(n^2).
- *  4. 对第 3 种方法进行优化，随机化输入。使得 O(N) 的时间复杂度得以保证 (BFPRT算法)
- *  该算法的一个典型应用便是 Top K 问题
- *  O(N) 时间复杂度 + O(1) 额外空间
- *  BFPRT算法的思想只是在选取 pivot 值时通过 random 的方法进行选取罢了，因此我们可以通过 shuffle 函数来对
- *  数组进行一次随机排序来实现。
- *  关于 BFPRT算法，想要进一步了解的可以查看：http://www.jianshu.com/p/a43b0e1712d1
- *  
- * Get more details here：https://discuss.leetcode.com/topic/14597/solution-explained	
+ *  4. 对第 3 种方法进行优化，随机化输入。使得均摊时间复杂度为 O(n)。
+ *  即我们可以随机选择一个值来作为我们的划分点 pivot.
+ *  为了保持代码的复用性，我们直接 随机选取了一个元素 与 数组的最后一个元素 进行了交换。其他代码保持不变即可。
+ *  其本身对应的也就是 随机快速排序 的 partition 方法。
+ *  5. BFPRT算法
+ *  关于 BFPRT算法，想要了解的可以查看：http://www.jianshu.com/p/a43b0e1712d1
+ *
+ * Get more details here：https://discuss.leetcode.com/topic/14597/solution-explained
  */
 
 /**
@@ -112,8 +112,8 @@ class Solution {
 }
 
 /**
- * Approach 2:  BFPRT
- * O(n) guaranteed running time + O(1) space
+ * Approach 2:  QuickSelect (Shuffle the Array)
+ * O(n) running time (Average) + O(1) space
  */
 class Solution {
     /*
@@ -130,7 +130,6 @@ class Solution {
             return 0;
         }
 
-        shuffle(nums);
         return helper(nums, 0, nums.length - 1, nums.length - k);
     }
 
@@ -154,6 +153,8 @@ class Solution {
 
     // Quick select: kth smallest
     public int partition(int[] nums, int l, int r) {
+        // swap a random value with nums[right] (shuffle the array)
+        swap(nums, l + (int) (Math.random() * (r - l + 1)), r);
         // 初始化左右指针和pivot
         int left = l, right = r;
         // Take nums[right] as the pivot
@@ -171,15 +172,6 @@ class Solution {
 
         // 返回当前pivot所在的index
         return left;
-    }
-
-    // 将数组元素打乱，使得 pivot 值的选取为随机
-    private void shuffle(int nums[]) {
-        final Random random = new Random();
-        for(int index = 1; index < nums.length; index++) {
-            final int r = random.nextInt(index + 1);
-            swap(nums, index, r);
-        }
     }
 
     private void swap(int[] nums, int i, int j) {
