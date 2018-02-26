57a59eef3c73ff83a77437cf78f2ad35ae23a166
cherryljr <travelerljr@gmail.com>
2018-02-26 13:12:58 +0900

    QuickSelect (Partition)

--- Sort Colors.java
+++ Sort Colors.java
@@ -21,7 +21,7 @@ Sort Array Two Pointers Facebook
 */
 
 /**
- * Approach: QuickSelect
+ * Approach: QuickSelect | Partition
  * 与 Partition Array / Sort Letters by Case 属于同一类题
  * 不同在于该题中需要针对 3 个元素进行排序。
  * 因此我们需要 3 个指针来进行排序，分别代表 0, 1, 2 即 red, white, blue
@@ -30,24 +30,20 @@ Sort Array Two Pointers Facebook
  * （将数组由原来的分成 2 个部分，改成分为 3 个部分。分别为：<pivot; =pivot; >pivot）
  *
  * 具体做法:
- * start 指向头节点 red , i 从头节点开始向后遍历 white, end 指向最后一个节点 blue
- * 当 a[i] = 0 时，说明为 red, 应排在最前面，故 swap(a[start], a[i])
- * 当 a[i] = 1 时，说明为 white, 应排在中间，故 i 继续向后移动
- * 当 a[i] = 2 时，说明为 blue, 应排在末尾，故 swap(a[i], a[end])
- * 直至 i 与 end 相遇或相交，则结束遍历。
- * 注意点：
- *  本题的 pivot 是外界给定的，所以我们需要一直遍历到 end (i <= end).
- *  而在 quickSort 中，我们的 pivot 选取的就是 a[end].
- *  因此我们只需要遍历到 pivot的右边界 即可 (i < right)，并且在 swap 的时候，是 swap(nums, i, --end).
- *  而在本题中，swap 的对象则是 swap(nums, i, end--).
- *  原因就是在于：
- *      quickSort 中的 pivot 已经被选定为 a[end].我们需要保持 a[end] 为 pivot.
- *      因此对于 >pivot 的元素，我们都会放在 (right, end-1) 上面。
- *      直到最后一步，将 等于pivot部分的右边界后的那个数 与 a[end](pivot) 交换，以完成 partition.
+ * 选定 pivot 为 white,大小为1.
+ * 定义 小于pivot部分的数组(red)的右边界 less 和 大于pivot部分数组(blue)的左边界 more.
+ * 然后利用指针 left 从 left 开始向 right 进行遍历。(直接利用了 left 的空间，省些空间哈）
+ *  当 nums[left] = 0 时，说明为 red, 应将该元素放在 小于pivot部分 中，
+ *  因此将 小于pivot部分的 下一个位置 与 nums[left] 进行交换（小于pivot部分的数组扩大） 即 swap(nums[++less], nums[left++])
+ *  当 nums[left] = 1 时，说明为 white, 应将该元素放在 等于pivot部分 中，故 left 继续向后移动 (left++)
+ *  当 nums[left] = 2 时，说明为 blue, 应将该元素放在 大于pivot部分 中，
+ *  因此将 大于pivot部分的 前一个位置 与 nums[left] 进行交换（大于pivot部分的数组扩大）故 swap(nums[--more], nums[left])
+ *  注意：交换后 nums[left] 位置上的数大小并不能确定(可能为 0,1,2),故需要再次进行判断，不能进行 left++ 的操作.
+ *  直至 left 与 more 相遇，说明已经遍历到大于pivot部分数组的边界了，则结束遍历。
  *
  * 算法时间复杂度为 O(N)； 时间复杂度为 O(1)
  */
-class Solution {
+public class Solution {
     /**
      * @param nums: A list of integer which is 0, 1 or 2
      * @return: nothing
@@ -57,17 +53,24 @@ class Solution {
             return;
         }
 
-        int start = 0;
-        int end = nums.length - 1;
-        int i = 0;
-        while (i <= end) {
-            if (nums[i] == 0) {
-                swap(nums, start++, i++);
-            } else if (nums[i] == 1) {
-                i++;
+        sortColorsHelper(nums, 0, nums.length - 1);
+    }
+
+    private void sortColorsHelper(int[] nums, int left, int right) {
+        int less = left - 1;    // 小于pivot部分的 右边界
+        int more = right + 1;   // 大于pivot部分的 左边界
+
+        while (left < more) {
+            if (nums[left] == 0) {
+                // 将 小于pivot部分的 下一个位置 与 nums[i] 进行交换（小于pivot部分的数组扩大）
+                swap(nums, ++less, left++);
+            } else if (nums[left] == 1) {
+                // 当 nums[i]==pivot 的时候，直接将 i 移动到下一个位置即可
+                left++;
             } else {
-                // 交换后 a[i] 位置上的数可能为 0 或 1,故需要再进行一次判断，不能进行 i++ 的操作
-                swap(nums, i, end--);
+                // 将 大于pivot部分的 前一个位置 与 nums[i] 进行交换（大于pivot部分的数组扩大）
+                // 交换后 a[i] 位置上的数大小并不能确定(可能为 0,1,2),故需要再次进行判断，不能进行 i++ 的操作
+                swap(nums, --more, left);
             }
         }
     }
@@ -78,3 +81,4 @@ class Solution {
         nums[j] = temp;
     }
 }
+
