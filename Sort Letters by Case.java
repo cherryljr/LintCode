d7595867ab9cf6c30ed32879c151f2411030dbbe
cherryljr <travelerljr@gmail.com>
2018-02-26 13:35:43 +0900

    Same as Partition Array (QuickSelect)

--- Sort Letters by Case.java
+++ Sort Letters by Case.java
@@ -1,16 +1,3 @@
-与 Partition Array 是相同的解法
-思路：
-	快速排序的思想，整个程序其实就是快速排序过程中的一次排序罢了。
-
-	利用两个指针left和right分别指向Array的起始和末尾。
-
-	利用while循环，left指针从左向右遍历，直到left指向的节点为 UpperCase.
-	同理right指针从右向左遍历，直到right指向的节点为 LowerCase.
-
-	交换left和right节点，直达left与right两个节点相遇或者相交。
-
-	算法复杂度为：O(N)
-
 /*
 Description
 Given a string which contains only letters. Sort it by lower case first and upper case second.
@@ -28,29 +15,44 @@ Tags
 Sort LintCode Copyright String Two Pointers
 */
 
+/**
+ * Approach: QuickSelect (Partition)
+ * 与 Partition Array 完全相同。
+ * 关于 Partition 函数的详细解析可以参考：
+ * https://github.com/cherryljr/LintCode/blob/master/Sort%20Colors.java
+ *
+ * 算法时间复杂度：O(n)； 空间复杂度：O(1)
+ */
 public class Solution {
-    /** 
+    /**
      *@param chars: The letter array you should sort by Case
      *@return: void
      */
     public void sortLetters(char[] chars) {
-        int left = 0;
-        int right = chars.length - 1;    
-        while (left <= right) {
-            while (left < right && Character.isLowerCase(chars[left])) {
-                left++;
-            }
-            while (left < right && Character.isUpperCase(chars[right])) {
-                right--;
-            }
-            
-            if (left <= right) {
-	            char temp = chars[left];
-	            chars[left] = chars[right];
-	            chars[right] = temp;
-	            left++;
-	            right--;
+        if (chars == null || chars.length < 2) {
+            return;
+        }
+
+        partition(chars, 0, chars.length - 1);
+    }
+
+    private void partition(char[] chars, int left, int right) {
+        // 初始化 小于k 和 大于k 部分数组的边界
+        int less = left - 1, more = right + 1;
+
+        // 当 left 指针遇到 大于k部分数组的 左边界 时停止遍历
+        while (left < more) {
+            if (Character.isLowerCase(chars[left])) {
+                swap(chars, ++less, left++);
+            } else {
+                swap(chars, --more, left);
             }
         }
+    }
+
+    private void swap(char[] chars, int i, int j) {
+        char temp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = temp;
     }
 }
