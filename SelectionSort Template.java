f6e401cd637f6dc0f30aa75e3b0eeee43e312972
cherryljr <travelerljr@gmail.com>
2018-03-01 20:08:36 +0900

    Template

--- SelectionSort Template.java
+++ SelectionSort Template.java
@@ -1,5 +1,5 @@
 /*
-Given an integer array, sort it in ascending order. Use bubble sort.
+Given an integer array, sort it in ascending order. Use selection sort algorithm.
 
 Example
 Given [3, 2, 1, 4, 5], return [1, 2, 3, 4, 5].
