53460d2a6dc87ffcdb63c3467a6191549b16cc68
cherryljr <travelerljr@gmail.com>
2018-03-06 13:14:44 +0900

    Binary Search

--- Search a 2D Matrix.java
+++ Search a 2D Matrix.java
@@ -1,38 +1,32 @@
 /*
+Description
 Write an efficient algorithm that searches for a value in an m x n matrix.
 
 This matrix has the following properties:
-
-    * Integers in each row are sorted from left to right.
-
-    * The first integer of each row is greater than the last integer of the previous row.
+    Integers in each row are sorted from left to right.
+    The first integer of each row is greater than the last integer of the previous row.
 
 Example
 Consider the following matrix:
-
 [
-
     [1, 3, 5, 7],
-
     [10, 11, 16, 20],
-
     [23, 30, 34, 50]
-
 ]
-
 Given target = 3, return true.
 
 Challenge
 O(log(n) + log(m)) time
-log(m * n) = log(m) + log(n)
 
-Tags Expand 
-Binary Search Matrix
-*/
+Tags
+Binary Search Matrix Yahoo
+ */
 
-
-
-// Binary Search Twice
+/**
+ * Approach 1: Binary Search Twice
+ * Firstly, using binary search in rows, find the ith row that the last number <= target
+ * Secondly, using binary search in cols, find the target number exists or not.
+ */
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0) {
@@ -41,11 +35,11 @@ public class Solution {
         if (matrix[0] == null || matrix[0].length == 0) {
             return false;
         }
-        
+
         int row = matrix.length;
         int column = matrix[0].length;
-        
-        // find the row index, the last number <= target 
+
+        // find the row index, the last number <= target
         int start = 0, end = row - 1;
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
@@ -64,7 +58,7 @@ public class Solution {
         } else {
             return false;
         }
-        
+
         // find the column index, the number equal to target
         start = 0;
         end = column - 1;
@@ -87,9 +81,17 @@ public class Solution {
     }
 }
 
-//  We can try to do Binary Search Once
-//	Thoughts: Convert into a 1D array, BinarySearch
+/**
+ * Approach 2: Binary Search (Only Once)
+ * We can Convert into a 1D array firstly, 
+ * Then use BinarySearch only once.
+ */
 public class Solution {
+    /*
+     * @param matrix: matrix, a list of lists of integers
+     * @param target: An integer
+     * @return: a boolean, indicate whether matrix contains target
+     */
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0) {
             return false;
@@ -97,72 +99,30 @@ public class Solution {
         if (matrix[0] == null || matrix[0].length == 0) {
             return false;
         }
-        
-        int row = matrix.length, column = matrix[0].length;
-        int start = 0, end = row * column - 1;
-        
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-            //	use mid / column to calculate the position in  row
-            // 	use mid % column to calculate the position in column
-            int number = matrix[mid / column][mid % column];
-            if (number == target) {
-                return true;
-            } else if (number < target) {
-                start = mid;
+
+        int rows = matrix.length, cols = matrix[0].length;
+        // Convert the matrix into a 1D Array
+        int left = 0, right = rows * cols - 1;
+        int row, col;
+        while (left < right) {
+            int mid = left + ((right - left) >> 2);
+            //	use mid / cols to calculate the position in  row
+            // 	use mid % cols to calculate the position in column
+            row = mid / cols;
+            col = mid % cols;
+            if (target <= matrix[row][col]) {
+                right = mid;
             } else {
-                end = mid;
+                left = mid + 1;
             }
         }
-        
-        if (matrix[start / column][start % column] == target) {
-            return true;
-        } else if (matrix[end / column][end % column] == target) {
-            return true;
-        }
-        
-        return false;
-    }
-}
 
-// Binary Search Once 
-// 更新后的模板写法
-public class Solution {
-    /**
-     * @param matrix, a list of lists of integers
-     * @param target, an integer
-     * @return a boolean, indicate whether matrix contains target
-     */
-    public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0) {
+        row = left / cols;
+        col = left % cols;
+        if (matrix[row][col] == target) {
+            return true;
+        } else {
             return false;
         }
-        
-        int row = matrix.length;
-        int column = matrix[0].length;
-        int start = 0; 
-        int end = row * column - 1;
-        int r, c;
-        
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            //	use mid / column to calculate the position in  row
-            // 	use mid % column to calculate the position in column
-            r = mid / column;
-            c = mid % column;
-            if (matrix[r][c] < target) {
-                start = mid + 1;
-            } else {
-                end = mid;
-            }
-        }
-        
-        r = start / column;
-        c = start % column;
-        if (matrix[r][c] == target) {
-            return true;
-        } 
-        
-        return false;
     }
 }
