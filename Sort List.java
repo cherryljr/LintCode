824fb9d9a94bbba2b247a57b3efc2748cccdac59
cherryljr <travelerljr@gmail.com>
2018-03-21 20:08:47 +0900

    Merge Sort | Quick Sort

--- Sort List.java
+++ Sort List.java
@@ -1,280 +1,146 @@
-这是一个排序问题，要求时间复杂度为O(nlogn),空间复杂度为常数。
-	解题思路如下：
-	1. 首先符合nlogn复杂度的排序有快速排序，归并排序以及堆排序。
-	2. 但是堆排序的额外空间复杂度不能满足要求。故只身下快速排序与归并排序。
-	接下来我们分别对二者进行分析。
-		
-Merge Sort:
-    1. find middle. 快慢指针
-    slow指向head, fast指向head.next。然后slow一次走一步, fast一次走两步。当fast走到链表的终点的时候，slow就处于链表的中间位置了。
-    2. Sort: 切开两半，先sort后半段,再sort前半段。
-        注意：在sort后半段之后，要将mid.next = null使得链表断开。不然head指向的仍然是整个链表
-    3. Merge: list A, B 已经是sorted, 然后按照大小，混合。
-    要recursively call itself.
-
-Quick Sort:
-	快速排序本身并不适合用在list上面。虽然这里也实现了它但是并不推荐大家使用它。
-	因为其随机性，故其最坏情况为O(n²)。
-	并且Quick Sort需要进行很多random access，但是LinkedList没办法像Arrays一样通过index直接进行访问而是要从head开始遍历。
-	
-	当应用在Array的时候Quick Sort虽然拥有O(1)额外空间的优点。
-	但是得益于LinkedList的特点：其插入操作的时间复杂度和空间复杂度均为：O(1).
-	故Meerge Sort中原本需要O(N)额外空间的merge操作只需要O(1)便可以完成.
-	
-综上所述，
-	Quick Sort不建议用在list上面。
-	排列list, Merge Sort可能更可行和合理。
-	
-	List更适合Merge Sort, Array更适合Quick Sort.
-	具体分析可以参见如下网页：
-	http://www.geeksforgeeks.org/why-quick-sort-preferred-for-arrays-and-merge-sort-for-linked-lists/
-
-
 /*
-28% Accepted
+Description
 Sort a linked list in O(n log n) time using constant space complexity.
 
 Example
-Given 1-3->2->null, sort it to 1->2->3->null.
+Given 1->3->2->null, sort it to 1->2->3->null.
 
-Tags Expand 
+Challenge 
+Solve it by merge sort & quick sort separately.
+
+Tags 
 Linked List
-
-
-*/
-
-
-// version 1: Merge Sort
+Related Problems 
+ */
 
 /**
- * Definition for ListNode.
+ * Intuition:
+ * 这是一个排序问题，要求时间复杂度为O(nlogn),空间复杂度为常数。
+ * 因此首先我们需要考虑的就是选取一个合适的排序方法。
+ * 经过分析，我们主要讨论 快速排序 和 归并排序 两种方法（它们的实质都是 分治思想）
+ * 接下来我们分别对二者进行分析。
+ *
+ * Merge Sort:
+ *  1. 首先我们需要找到 中点 作为划分点，find middle. 使用到了 快慢指针 的知识点
+ *  slow, fast指向head。然后slow一次走一步, fast一次走两步。当fast走到链表的终点的时候，slow就处于链表的中间位置了。
+ *  2. Sort: 切开两半，先sort后半段,再sort前半段（递归调用）。
+ *  注意：在sort后半段之后，要将 mid.next = null 使得链表断开。不然 head 指向的仍然是整个链表
+ *  3. Merge: list A, B 已经是sorted, 然后按照大小 merge 起来即可。
+ *  （可以参考 Merge Two Sorted List: https://github.com/cherryljr/LintCode/blob/master/Merge%20Two%20Sorted%20List.java）
+ *
+ * 关于利用 快慢指针 寻找中点：
+ *  注意本题的代码实现：代码中我们保存了一个 pre 节点，它表示的是 slow 的前一个节点。
+ *  如果我们返回的是 slow 的话，在 slow.next = null 之后，如果链表被划分到只剩下 2个 节点时，
+ *  slow 会一直指向 第二个节点（末尾节点），这样就会导致最后这两个节点无法被 Divide,从而陷入无限循环，导致 Stack Overflow.
+ *  关于这个，其实有更加简单的方法：
+ *      在初始化 slow,fast 的时候，我们将 slow = head, fast = head.next 即可。
+ *      此时我们得到的 slow 就是链表的中点（靠前），为什么说靠前呢？
+ *      这是因为当链表节点为偶数个时，它返回的是第 size/2 个节点.
+ *      并且当只有 2个 节点时，它就不会陷入无限循环的情况了.
+ *  当我们将初始化改为：slow = head, fast = head 的时候。
+ *      我们得到的 slow 就是链表的中点（靠后），
+ *      同理即 当链表节点为偶数个时，它返回的是第 size/2 + 1 个节点.
+ *  具体代码见下方，因此我们可以根据情况的不同选择不同的实现方式。
+ *  （然而就实际应用而已我们基本都是采用 slow = head, fast = head. 的方法，如果需要取之前的一个节点，那么使用 pre 即可）
+ *
+ * Quick Sort:
+ * 快速排序本身并不适合用在list上面。虽然这里也实现了它但是并不推荐大家使用它。
+ * 因为其随机性，故其最坏情况为O(n²)。
+ * 并且Quick Sort需要进行很多random access，但是LinkedList没办法像ArrayList一样 通过index直接进行访问 而是要从head开始遍历。
+ *
+ * 当应用在Array的时候Quick Sort虽然拥有O(logn)额外空间的优点。
+ * 但是得益于LinkedList的特点：其插入操作的时间复杂度和空间复杂度均为：O(1).
+ * 故Merge Sort中原本需要 O(n) 额外空间的merge操作只需要 O(1) 便可以完成.
+ *
+ * 综上所述，
+ * Quick Sort不建议用在list上面。
+ * LinkedList更适合Merge Sort, ArrayList更适合Quick Sort.
+ * 具体分析可以参见如下网页：
+ * http://www.geeksforgeeks.org/why-quick-sort-preferred-for-arrays-and-merge-sort-for-linked-lists/
+ */
+
+/**
+ * Approach 1: Merge Sort (Divide and Conquer)
+ */
+/**
+ * Definition for ListNode
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int val) {
- *         this.val = val;
- *         this.next = null;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
  *     }
  * }
- */ 
-
-public class Solution {            
-    private ListNode findMiddle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head.next;
-        
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        
-        return slow;
-    }    
-
-    private ListNode merge(ListNode head1, ListNode head2) {
-       ListNode dummy = new ListNode(-1);
-       ListNode node = dummy;
-       
-       while (head1 != null && head2 != null) {
-           if (head1.val < head2.val) {
-               node.next = head1;
-               head1 = head1.next;
-           } else {
-               node.next = head2;
-               head2 = head2.next;
-           }
-           node = node.next;
-       }
-       
-       if (head1 != null) {
-           node.next = head1;
-       } 
-       if (head2 != null) {
-           node.next = head2;
-       }
-       
-       return dummy.next;
-    }
-
-    public ListNode sortList(ListNode head) {
-       if (head == null || head.next == null) {
-           return head;
-       }
-       
-       ListNode mid = findMiddle(head);
-       //	注意后半段是从mid.next开始.mid是属于前半段的
-       ListNode right = sortList(mid.next);
-       mid.next = null;
-       ListNode left = sortList(head);
-       
-       return merge(left, right);
-    }
-}
-
-// version 2: Quick Sort 1
-public class Solution {
-    public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        
-        ListNode mid = findMedian(head); // O(n)
-        
-        ListNode leftDummy = new ListNode(0), leftTail = leftDummy;
-        ListNode rightDummy = new ListNode(0), rightTail = rightDummy;
-        ListNode middleDummy = new ListNode(0), middleTail = middleDummy;
-        while (head != null) {
-            if (head.val < mid.val) {
-                leftTail.next = head;
-                leftTail = head;
-            } else if (head.val > mid.val) {
-                rightTail.next = head;
-                rightTail = head;
-            } else {
-                middleTail.next = head;
-                middleTail = head;
-            }
-            head = head.next;
-        }
-        
-        leftTail.next = null;
-        middleTail.next = null;
-        rightTail.next = null;
-        
-        ListNode left = sortList(leftDummy.next);
-        ListNode right = sortList(rightDummy.next);
-        
-        return concat(left, middleDummy.next, right);
-    }
-    
-    private ListNode findMedian(ListNode head) {
-        ListNode slow = head, fast = head.next;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-    }
-    
-    private ListNode concat(ListNode left, ListNode middle, ListNode right) {
-        ListNode dummy = new ListNode(0), tail = dummy;
-        
-        tail.next = left; tail = getTail(tail);
-        tail.next = middle; tail = getTail(tail);
-        tail.next = right; tail = getTail(tail);
-        return dummy.next;
-    }
-    
-    private ListNode getTail(ListNode head) {
-        if (head == null) {
-           return null;
-        } 
-       
-        while (head.next != null) {
-            head = head.next;
-        }
-        return head;
-    }
-}
-
-// version 3: Quick Sort 2
-/**
- * Definition for ListNode.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int val) {
- *         this.val = val;
- *         this.next = null;
- *     }
- * }
- */ 
-class Pair {
-    public ListNode first, second; 
-    public Pair(ListNode first, ListNode second) {
-        this.first = first;
-        this.second = second;
-    }
-}
-
+ */
 public class Solution {
     /**
      * @param head: The head of linked list.
-     * @return: You should return the head of the sorted linked list,
-                    using constant space complexity.
+     * @return: You should return the head of the sorted linked list, using constant space complexity.
      */
     public ListNode sortList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
-        
-        ListNode mid = findMedian(head); // O(n)
-        Pair pair = partition(head, mid.val); // O(n)
-        
-        ListNode left = sortList(pair.first);
-        ListNode right = sortList(pair.second);
-        
-        getTail(left).next = right; // O(n)
-        
-        return left;
+
+        ListNode mid = findPreMid(head);
+        // Sort the right part of mid (recursive call)
+        ListNode right = sortList(mid.next);
+        // Split the list from middle node
+        mid.next = null;
+        // Sort the left part of mid (recursive call)
+        ListNode left = sortList(head);
+
+        return merge(left, right);
     }
-    
-    // 1->2->3 return 2
-    // 1->2 return 1
-    private ListNode findMedian(ListNode head) {
-        ListNode slow = head, fast = head.next;
+
+    private ListNode findPreMid(ListNode head) {
+        ListNode pre = null;    // store the previous node of slow
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return pre;
+    }
+
+    private ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
+
         return slow;
     }
-    
-    // < value in the left, > value in the right
-    private Pair partition(ListNode head, int value) {
-        ListNode leftDummy = new ListNode(0), leftTail = leftDummy;
-        ListNode rightDummy = new ListNode(0), rightTail = rightDummy;
-        ListNode equalDummy = new ListNode(0), equalTail = equalDummy;
-        while (head != null) {
-            if (head.val < value) {
-                leftTail.next = head;
-                leftTail = head;
-            } else if (head.val > value) {
-                rightTail.next = head;
-                rightTail = head;
+
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode node = dummy;
+
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
+                node.next = head1;
+                head1 = head1.next;
             } else {
-                equalTail.next = head;
-                equalTail = head;
+                node.next = head2;
+                head2 = head2.next;
             }
-            head = head.next;
+            node = node.next;
         }
-        
-        leftTail.next = null;
-        rightTail.next = null;
-        equalTail.next = null;
-        
-        if (leftDummy.next == null && rightDummy.next == null) {
-            ListNode mid = findMedian(equalDummy.next);
-            leftDummy.next = equalDummy.next;
-            rightDummy.next = mid.next;
-            mid.next = null;
-        } else if (leftDummy.next == null) {
-            leftTail.next = equalDummy.next;
-        } else {
-            rightTail.next = equalDummy.next;
+
+        if (head1 != null) {
+            node.next = head1;
         }
-        
-        return new Pair(leftDummy.next, rightDummy.next);
-    }
-    
-    private ListNode getTail(ListNode head) {
-        if (head == null) {
-           return null;
-        } 
-       
-        while (head.next != null) {
-            head = head.next;
+        if (head2 != null) {
+            node.next = head2;
         }
-        return head;
+
+        return dummy.next;
     }
 }
