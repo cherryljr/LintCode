5ed77751aaba8021089bd68678d851796728e8e7
cherryljr <travelerljr@gmail.com>
2018-03-26 21:50:38 +0900

    PreSum + Sort

--- Subarray Sum Closest.java
+++ Subarray Sum Closest.java
@@ -1,16 +1,3 @@
-Subarray 首先联想到的就是要使用 Prefix Sum 来解决问题。
-该题是在 Subarray Sum 基础上的扩展。
-思路：
-	在 Subarray Sum 中，我们通过了 sum[0~a] = sum[0~b] = x 得到了 sum(a ~ b] = 0.    
-	而本题需要的是与 0 最相近的。故同样借助 Prefix Sum,只不过我们需要的是将各个 Prefix Sum 
-	之间的差值尽可能小地排列在一起方便我们查找。而能做到这点的便是 排序。
-做法：
-	1. 我们需要同时保存 Sum 与 index 两个数值，故我们新建了一个类 Pair 来存储它们。
-	2. 遍历整个数组，算出每个位置上的 Sum 以及对应的 index 并保存到 Pair 中。
-	3. 对所有得到的 Sum，依据 Sum 的大小进行 Sort (O(NlogN))
-	4. 遍历得到的 Pair 数组，通过 sums[i].sum - sums[i-1].sum 来比较得到最接近 0 的 Subarray
-	5. 最后对 index 进行一次 sort 便可以得到最后答案。
-
 /*
 Description
 Given an integer array, find a subarray with sum closest to zero. Return the indexes of the first number and last number.
@@ -25,55 +12,74 @@ Tags
 Sort Subarray
 */
 
-class Pair {
-    int sum;
-    int index;
-    public Pair(int s, int i) {
-        sum = s;
-        index = i;
-    }
-}
-    
+/**
+ * Approach: PreSum + Sorting
+ * 涉及到 Subarray Sum 问题，首先联想到的就是要使用 Prefix Sum 来解决问题。
+ * 该题是在 Subarray Sum 基础上的扩展 / 累加和为K的最长子数组 的变形。
+ * https://github.com/cherryljr/NowCoder/blob/master/%E7%B4%AF%E5%8A%A0%E5%92%8C%E4%B8%BAK%E7%9A%84%E6%9C%80%E9%95%BF%E5%AD%90%E6%95%B0%E7%BB%84.java
+ *
+ * 思路：
+ * 在 Subarray Sum 中，我们通过了 sum[0~a] = sum[0~b] = x 得到了 sum(a ~ b] = 0.
+ * 而本题需要的是与 0 最相近的。故同样借助 Prefix Sum,只不过我们需要的是将各个 Prefix Sum
+ * 之间的差值尽可能小地排列在一起方便我们查找。而能做到这点的便是 排序。
+ * 因此我们这里不再使用 HashMap 而是使用了 Array 来实现。
+ * 但是因为 Array 无法像 HashMap 储存一个对应的键值对，所以我们需要新建一个类 Point 来储存它们。
+ * （当然使用简单的二维数组也可以，这边是为了使得代码更加容易理解）
+ * 
+ * 做法：
+ *  1. 新建 Point 类，包含 preSum 和 index。
+ *  2. 遍历整个数组，算出每个位置上的 preSum 以及对应的 index 并保存到 map 中。
+ *  3. 对所有得到的 preSum，依据 preSum 的大小进行 Sort (O(nlogn))
+ *  4. 遍历得到的 map 数组，通过 map[i].preSum - map[i-1].preSum 来比较得到最接近 0 的 Subarray
+ *  5. 最后对 index 进行一次 sort 便可以得到最后答案。
+ */
 public class Solution {
-    /**
+
+    class Point {
+        int preSum;
+        int index;
+
+        Point(int preSum, int index) {
+            this.preSum = preSum;
+            this.index = index;
+        }
+    }
+
+    /*
      * @param nums: A list of integers
-     * @return: A list of integers includes the index of the first number 
-     *          and the index of the last number
+     * @return: A list of integers includes the index of the first number and the index of the last number
      */
     public int[] subarraySumClosest(int[] nums) {
-        int[] res = new int[2];
         if (nums == null || nums.length == 0) {
-            return res;
-        } 
-        int len = nums.length;
-        if(len == 1) {
-            res[0] = res[1] = 0;
-            return res;
+            return new int[]{};
         }
-        
-        Pair[] sums = new Pair[len+1];
-        sums[0] = new Pair(0, 0);
-        for (int i = 1; i <= len; i++) {
-            sums[i] = new Pair(sums[i-1].sum + nums[i-1], i);
+
+        Point[] map = new Point[nums.length + 1];
+        // 初始化 map (当一个元素都没有的时候，preSum为0，index为-1)
+        map[0] = new Point(0, -1);
+        int preSum = 0;
+        // 因为我们需要对 preSum 进行排序，所以使用 数组 来存储preSum和相应的位置.
+        for (int i = 0; i < nums.length; i++) {
+            preSum += nums[i];
+            map[i + 1] = new Point(preSum, i);
         }
-        
-        Arrays.sort(sums, new Comparator<Pair>() {
-           public int compare(Pair a, Pair b) {
-               return a.sum - b.sum;
-           } 
-        });
-        
-        int ans = Integer.MAX_VALUE;
-        for (int i = 1; i <= len; i++) {
-            if (ans > sums[i].sum - sums[i-1].sum) {
-                ans = sums[i].sum - sums[i-1].sum;
-                int[] temp = new int[]{sums[i].index - 1, sums[i-1].index - 1};
+
+        // 对 Point 按从小到达的顺序进行排序
+        Arrays.sort(map, (a, b) ->
+                a.preSum == b.preSum ? a.index - b.index : a.preSum - b.preSum);
+        int[] rst = new int[2];
+        int diff = Integer.MAX_VALUE;
+        for (int i = 1; i < map.length; i++) {
+            if (map[i].preSum - map[i - 1].preSum < diff) {
+                // 当发现 相邻差值更小的 preSum 时，更新 diff 和 子数组左右边界
+                diff = map[i].preSum - map[i - 1].preSum;
+                int[] temp = new int[]{map[i].index, map[i - 1].index};
                 Arrays.sort(temp);
-                res[0] = temp[0] + 1;
-                res[1] = temp[1];
+                rst[0] = temp[0] + 1;
+                rst[1] = temp[1];
             }
         }
-        
-        return res;
+
+        return rst;
     }
 }
