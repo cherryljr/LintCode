74e06112ef3ed2563e8fcab59bf7fb9b1e99b5e6
cherryljr <travelerljr@gmail.com>
2018-04-01 17:39:30 +0900

    DoubleLinkedList + HashMap | LinkedHashMap

--- LRU Cache.java
+++ LRU Cache.java
@@ -1,45 +1,101 @@
-思路过程：
-首先对题目进行需求分析，该程序需要具备以下功能：
-1.	删除头元素
-2.	尾巴插入一个元素
-3.	删除中间元素
-4.	元素按照时间排序
-5.	快速查找一个元素
-于是我们想到了使用 链表 / 数组 来实现。因为需要经常性地增加或删除元素
-故我们排除了数组，但是链表又无法做到快速查找一个元素。所以我们想到了能否借助
-HashMap来实现快速查找。
-每当一个新的元素到来时，我们先查询HashMap,如果表里已经有这个key了,则更新该链表，
-将该元素移动到链表末尾，表示该元素刚刚被访问过。即链表的顺序就是元素被访问的先后顺序。
-如果不包含该元素这将其插入链表的末尾，如果空间已经满了，则删除链表的一个节点。然后再
-插入该元素。
-
-注：1. 为了方便操作，链表拥有头节点和尾节点, 作用类似 Dummy Node
-		2. 虽然HashMap能帮我们快速定位到要查找的节点，但是链表要删除一个节点需要知道
-		其前面以及后面一个的节点是什么。所以我们使用了双向链表。
-		涉及到的知识点：双向链表的 删除 与 插入. （画图分析，先做好一个方向）
-	
 /*
 Description
-Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and set.
+Design and implement a data structure for Least Recently Used (LRU) cache.
+It should support the following operations: get and set.
 
 get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
-set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, 
-it should invalidate the least recently used item before inserting a new item.
+set(key, value) - Set or insert the value if the key is not already present.
+When the cache reached its CAPACITY, it should invalidate the least recently used item before inserting a new item.
 
-Tags 
-Linked List Google Uber Zenefits
-*/
+Example
+Tags
+Linked List Uber Zenefits Google
+ */
 
-// Use double linked list to store value.
-// Store key in hashmap<key, node> to find node quickly
-
+/**
+ * Approach 1: HashMap + DoubleLinkedList
+ * 这类题目纯粹考察的是我们的 Coding 能力。
+ * 只需要分析好这个数据结构需要实现的 逻辑功能，并处理好各种情况下的 Case,就能够解决题目。
+ *
+ * 为了实现 LRU，首先我们需要准备一个 HashMap 来存储 key 与对应的信息 value。
+ * 其次，各个节点具有优先级（最新被调用的优先级最高），并且我们需要 频繁地 对其进行修改。
+ * 那么最合适的毫无疑问就是 链表了。并且我们还需要在队列的两端都能够进行操作，
+ * 因此我们需要设计一个 双向链表。具备 head, tail 指针，数据从 尾部 插入（tail节点优先级最高）
+ *
+ * 接下来我们来考虑，HashMap 和 链表的节点 中应该存放哪些信息。
+ * 对与 HashMap 而言，key毫无疑问就是我们插入的 key 了，但是 value 就直接是对应的 value 吗？
+ * 并不是，因为对于 链表 而言，查询一个 key 需要花费 O(n) 的时间代价，
+ * 但是我们使用了 HashMap 因此可以将查询的时间复杂度降低到 O(1),那么为什么不在 value 中放入链表的 Node 呢？
+ * 这样就能够使我们可以通过 map 查询到 key 对应的 node 之后，直接对 双向链表 进行操作，从而省去遍历链表的时间。
+ * 对于 Node 而言，我们需要放入的信息有：value, prePointer, nextPointer 以及 key.
+ * 放入 key 的原因是考虑到了 removeHead() 这个函数，当 LRU 的大小到达 CAPACITY 时，
+ * 如果继续插入元素，我们将移除 优先级最低 的节点，也就是我们的 head 节点。
+ * 这个时候如果 Node 中含有 key 这个信息的话，我们可以直接通过 Node 获得 key，从而对 map 也进行相应的 remove 操作。
+ *
+ * 具体实现请参考代码以及注释。
+ */
 public class LRUCache {
-    private class Node{
-        Node pre;
-        Node next;
+    private Map<Integer, Node> map;
+    private DoubleLinkedList list;
+    public final int CAPACITY;
+
+    /*
+     * @param capacity: An integer
+     */public LRUCache(int capacity) {
+        // do intialization if necessary
+        this.map = new HashMap<>();
+        this.list = new DoubleLinkedList();
+        this.CAPACITY = capacity;
+    }
+
+    /*
+     * @param key: An integer
+     * @return: An integer
+     */
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            // 因为发生 get 操作，将对应的节点移动到链表末尾（优先级置为最高）
+            list.moveNodeToTail(node);
+            return node.value;
+        }
+        return -1;
+    }
+
+    /*
+     * @param key: An integer
+     * @param value: An integer
+     * @return: nothing
+     */
+    public void set(int key, int value) {
+        if (!map.containsKey(key)) {
+            // 若不包含该元素，则在 map 和 list 中插入数据
+            Node newNode = new Node(key, value);
+            list.add(newNode);
+            map.put(key, newNode);
+            if (map.size() > CAPACITY) {
+                // 如果本次插入导致了map的大小 大于 LRU.CAPACITY，则移除优先级最低的节点
+                removeMostUnusedNode();
+            }
+        } else {
+            // 更新节点值，同时将对应的节点优先级置为最高
+            Node node = map.get(key);
+            node.value = value;
+            list.moveNodeToTail(node);
+        }
+    }
+
+    private void removeMostUnusedNode() {
+        // 分别从 list 和 map 中移除对应的元素
+        Node removedNode = this.list.removeHead();
+        map.remove(removedNode.key);
+    }
+
+    private class Node {
         int key;
         int value;
-    
+        Node pre, next;
+
         public Node(int key, int value) {
             this.key = key;
             this.value = value;
@@ -47,62 +103,258 @@ public class LRUCache {
             this.next = null;
         }
     }
-    
-    private int capacity;
-    private HashMap<Integer, Node> hs = new HashMap<Integer, Node>();
-    private Node head = new Node(-1, -1);
-    private Node tail = new Node(-1, -1);
 
-    // @param capacity, an integer
+    private class DoubleLinkedList {
+        private Node head;  // 头结点（优先级最低）
+        private Node tail;  // 尾节点（优先级最高）
+
+        public DoubleLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void add(Node node) {
+            if (node == null) {
+                return;
+            }
+
+            if (head == null) {
+                head = node;
+                tail = node;
+            } else {
+                tail.next = node;
+                node.pre = tail;
+                tail = node;
+            }
+        }
+
+        public void moveNodeToTail(Node node) {
+            if (node == tail) {
+                // 如果本身就是尾节点，则无需移动
+                return;
+            }
+
+            if (node == head) {
+                // 如果要移动到末尾的是头节点，则需要将头结点向后移动一位（移除原来的头结点）
+                head = head.next;
+                head.pre = null;
+            } else {
+                // 如果要移除的节点位于 链表中间，则连接该节点两端的节点
+                node.pre.next = node.next;
+                node.next.pre = node.pre;
+            }
+            // 将该节点连接在 原来的tail 之后，然后更新 tail 节点
+            tail.next = node;
+            node.pre = tail;
+            node.next = null;
+            tail = node;
+        }
+
+        public Node removeHead() {
+            if (head == null) {
+                return null;
+            }
+
+            Node removedNode = head;
+            if (head == tail) {
+                // 只有一个节点，直接删除即可
+                head = null;
+                tail = null;
+            } else {
+                // 删除头节点
+                head = removedNode.next;
+                head.pre = null;
+                removedNode.next = null;
+            }
+            return removedNode;
+        }
+    }
+}
+
+/**
+ * Approach 2: Using LinkedHashMap
+ * This is the laziest implementation using Java’s LinkedHashMap.
+ *
+ * Several points to mention:
+ * In the constructor, the third boolean parameter specifies the ordering mode.
+ * If we set it to true, it will be in access order.
+ * (https://docs.oracle.com/javase/8/docs/api/java/util/LinkedHashMap.html#LinkedHashMap-int-float-boolean-)
+ * By overriding removeEldestEntry in this way, we do not need to take care of it ourselves.
+ * It will automatically remove the least recent one when the size of map exceeds the specified capacity.
+ * (https://docs.oracle.com/javase/8/docs/api/java/util/LinkedHashMap.html#removeEldestEntry-java.util.Map.Entry-)
+ */
+public class LRUCache {
+    private LinkedHashMap<Integer, Integer> map;
+    private final int CAPACITY;
+
+    /*
+     * @param capacity: An integer
+     */
     public LRUCache(int capacity) {
-        this.capacity = capacity;
-        tail.pre = head;
-        head.next = tail;
+        // do intialization if necessary
+        this.CAPACITY = capacity;
+        // The map will be in access order
+        map = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true){
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > CAPACITY;
+            }
+        };
     }
 
-    // @return an integer
+    /*
+     * @param key: An integer
+     * @return: An integer
+     */
     public int get(int key) {
-        if( !hs.containsKey(key)) {
-            return -1;
-        }
-
-        // remove current
-        Node current = hs.get(key);
-        current.pre.next = current.next;
-        current.next.pre = current.pre;
-
-        // move current to tail
-        move_to_tail(current);
-
-        return hs.get(key).value;
+        return map.getOrDefault(key, -1);
     }
 
-    // @param key, an integer
-    // @param value, an integer
-    // @return nothing
+    /*
+     * @param key: An integer
+     * @param value: An integer
+     * @return: nothing
+     */
     public void set(int key, int value) {
-        // this internal `get` method will update the key's position in the linked list.
-        if (get(key) != -1) {
-            hs.get(key).value = value;
-            return;
+        map.put(key, value);
+    }
+}
+
+
+/**
+ * LRU Cache Template (Using Generics)
+ */
+public class LRU {
+
+    public static class Node<K, V> {
+        public K key;
+        public V value;
+        public Node<K, V> pre;
+        public Node<K, V> next;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.pre = null;
+            this.next = null;
+        }
+    }
+
+    public static class DoubleLinkedList<K, V> {
+        private Node<K, V> head;
+        private Node<K, V> tail;
+
+        public DoubleLinkedList() {
+            this.head = null;
+            this.tail = null;
         }
 
-        if (hs.size() == capacity) {
-            hs.remove(head.next.key);
-            head.next = head.next.next;
-            head.next.pre = head;
+        public void addNode(Node<K, V> newNode) {
+            if (newNode == null) {
+                return;
+            }
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                newNode.pre = tail;
+                tail = newNode;
+            }
         }
 
-        Node insert = new Node(key, value);
-        hs.put(key, insert);
-        move_to_tail(insert);
+        public void moveNodeToTail(Node<K, V> node) {
+            if (tail == node) {
+                return;
+            }
+
+            if (head == node) {
+                head = node.next;
+                head.pre = null;
+            } else {
+                node.pre.next = node.next;
+                node.next.pre = node.pre;
+            }
+            tail.next = node;
+            node.pre = tail;
+            node.next = null;
+            tail = node;
+        }
+
+        public Node<K, V> removeHead() {
+            if (head == null) {
+                return null;
+            }
+
+            Node<K, V> removedNode = head;
+            if (head == tail) {
+                head = null;
+                tail = null;
+            } else {
+                head = removedNode.next;
+                head.pre = null;
+                removedNode.next = null;
+            }
+            return removedNode;
+        }
+
     }
-    // 双向链表的插入画图再写，可以保证清晰不会乱
-    // 顺序为：从插入节点开始，先保证一个方向全部改好，然后再去改另一个方向
-     private void move_to_tail(Node current) {
-        current.pre = tail.pre;
-        tail.pre = current;
-        current.pre.next = current;
-        current.next = tail;
+
+    public static class LRUCache<K, V> {
+        private HashMap<K, Node<K, V>> map;
+        private DoubleLinkedList<K, V> list;
+        private final int CAPACITY;
+
+        public LRUCache(int capacity) {
+            // do intialization
+            if (capacity < 1) {
+                throw new RuntimeException("should be more than 0.");
+            }
+            map = new HashMap<>();
+            list = new DoubleLinkedList<>();
+            this.CAPACITY = capacity;
+        }
+
+        public V get(K key) {
+            if (map.containsKey(key)) {
+                Node<K, V> node = map.get(key);
+                list.moveNodeToTail(node);
+                return node.value;
+            }
+            return null;
+        }
+
+        public void set(K key, V value) {
+            if (map.containsKey(key)) {
+                Node<K, V> node = map.get(key);
+                node.value = value;
+                list.moveNodeToTail(node);
+            } else {
+                Node<K, V> newNode = new Node<>(key, value);
+                map.put(key, newNode);
+                list.addNode(newNode);
+                if (map.size() > CAPACITY) {
+                    removeMostUnusedCache();
+                }
+            }
+        }
+
+        private void removeMostUnusedCache() {
+            Node<K, V> removedNode = list.removeHead();
+            map.remove(removedNode.key);
+        }
+
     }
+
+    public static void main(String[] args) {
+        LRUCache<String, Integer> testCache = new LRUCache<>(3);
+        testCache.set("A", 1);
+        testCache.set("B", 2);
+        testCache.set("C", 3);
+        System.out.println(testCache.get("B"));
+        System.out.println(testCache.get("A"));
+        testCache.set("D", 4);
+        System.out.println(testCache.get("D"));
+        System.out.println(testCache.get("C"));
+    }
+
 }
