8ca65ad8bb8dc26051a7aa835e7723c7072dcfc6
cherryljr <travelerljr@gmail.com>
2018-04-07 16:29:38 +0900

    Divide and Conquer | PriorityQueue

--- Merge k Sorted Lists.java
+++ Merge k Sorted Lists.java
@@ -1,30 +1,10 @@
-有三种解决方案：
-1. Divide & Conquer
-
-2. 使用优先级队列（基于堆排序实现）————— 推荐此种解法
-	 用Priorityqueue来排列所有list的leading node.   
-
-	 时间：n*O(logk)   
-	 PriorityQueue: logk   
-
-3. merge two by two
-
-这个题目可以有好几个衍生：   
-   比如，如果k很大，一个机器上放不下所有的k list怎么办？ 
-   比如，如果Merge起来的很长，一个机器上放不下怎么办？
-
-```
-
 /*
-
+Description
 Merge k sorted linked lists and return it as one sorted list.
-
 Analyze and describe its complexity.
-
 
 Example
 Given lists:
-
 [
   2->4->null,
   null,
@@ -32,13 +12,21 @@ Given lists:
 ],
 return -1->2->4->null.
 
-Tags Expand 
-Divide and Conquer Linked List Priority Queue Heap Uber Google Twitter LinkedIn Airbnb Facebook
+Tags
+Facebook Priority Queue Divide and Conquer Heap Uber Google Twitter LinkedIn Airbnb Linked List Microsoft Amazon IXL
+ */
 
-
-*/
-
-// version 1: Divide & Conquer
+/**
+ * Approach 1: Divide and Conquer
+ * 使用 分治 的思想，利用 mergeTwoLists 这个方法逐渐将两个 sorted list 合并起来。
+ *
+ * 这个题目可以有好几个衍生：
+ * 比如，如果k很大，一个机器上放不下所有的k list怎么办？
+ * 比如，如果Merge起来的很长，一个机器上放不下怎么办？
+ *
+ * More Approaches and Detail Explanations:
+ * https://leetcode.com/articles/merge-k-sorted-list/
+ */
 
 /**
  * Definition for ListNode.
@@ -60,134 +48,81 @@ public class Solution {
         if (lists.size() == 0) {
             return null;
         }
+
         return mergeHelper(lists, 0, lists.size() - 1);
     }
-    
+
     private ListNode mergeHelper(List<ListNode> lists, int start, int end) {
         if (start == end) {
             return lists.get(start);
         }
-        
+
         int mid = start + (end - start) / 2;
         ListNode left = mergeHelper(lists, start, mid);
         ListNode right = mergeHelper(lists, mid + 1, end);
         return mergeTwoLists(left, right);
     }
-    
+
     private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
+        ListNode curr = dummy;
         while (list1 != null && list2 != null) {
             if (list1.val < list2.val) {
-                tail.next = list1;
-                tail = list1;
+                curr.next = list1;
+                curr = list1;
                 list1 = list1.next;
             } else {
-                tail.next = list2;
-                tail = list2;
+                curr.next = list2;
+                curr = list2;
                 list2 = list2.next;
             }
         }
         if (list1 != null) {
-            tail.next = list1;
+            curr.next = list1;
         } else {
-            tail.next = list2;
+            curr.next = list2;
         }
-        
+
         return dummy.next;
     }
 }
 
-
-// version 2: Heap
-
+/**
+ * Approach 2: PriorityQueue
+ * 使用优先级队列（基于堆排序实现）————— 推荐此种解法
+ * 用 PriorityQueue 来排列所有list的leading node.
+ * （在将 node 加入到堆之前，记得判断 node 是否为空）
+ *
+ * 时间复杂度：O(nlogk)
+ */
 public class Solution {
     /**
      * @param lists: a list of ListNode
      * @return: The head of one sorted list.
      */
-    public ListNode mergeKLists(List<ListNode> lists) {  
-        // write your code here
+    public ListNode mergeKLists(List<ListNode> lists) {
         if (lists == null || lists.size() == 0) {
             return null;
         }
-        
-        PriorityQueue<ListNode> heap = 
-        new PriorityQueue<ListNode>(lists.size(), new Comparator<ListNode>(){
-            public int compare(ListNode a, ListNode b) {
-                return a.val - b.val;
-            }
-        });
-        for (int i = 0; i < lists.size(); i++) {
-            if (lists.get(i) != null) {
-                heap.offer(lists.get(i));
+
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> (a.val - b.val));
+        for (ListNode node : lists) {
+            if (node != null) {
+                minHeap.offer(node);
             }
         }
-        
+
         ListNode dummy = new ListNode(-1);
-        ListNode tail = dummy;
-        
-        while (!heap.isEmpty()) {
-            ListNode head = heap.poll();
-            tail.next = head;
-            tail = head;
-            if (head.next != null) {
-                heap.offer(head.next);
+        ListNode curr = dummy;
+        while (!minHeap.isEmpty()) {
+            ListNode node = minHeap.poll();
+            curr.next = node;
+            curr = node;
+            if (node.next != null) {
+                minHeap.offer(node.next);
             }
         }
+
         return dummy.next;
     }
 }
-
-
-// Version 3: merge two by two
-
-public class Solution {
-    /**
-     * @param lists: a list of ListNode
-     * @return: The head of one sorted list.
-     */
-    public ListNode mergeKLists(List<ListNode> lists) {  
-        if (lists == null || lists.size() == 0) {
-            return null;
-        }
-        
-        while (lists.size() > 1) {
-            List<ListNode> new_lists = new ArrayList<ListNode>();
-            for (int i = 0; i + 1 < lists.size(); i += 2) {
-                ListNode merged_list = merge(lists.get(i), lists.get(i+1));
-                new_lists.add(merged_list);
-            }
-            if (lists.size() % 2 == 1) {
-                new_lists.add(lists.get(lists.size() - 1));
-            }
-            lists = new_lists;
-        }
-        
-        return lists.get(0);
-    }
-    
-    private ListNode merge(ListNode a, ListNode b) {
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-        while (a != null && b != null) {
-            if (a.val < b.val) {
-                tail.next = a;
-                a = a.next;
-            } else {
-                tail.next = b;
-                b = b.next;
-            }
-            tail = tail.next;
-        }
-        
-        if (a != null) {
-            tail.next = a;
-        } else {
-            tail.next = b;
-        }
-        
-        return dummy.next;
-    }
-}
-
