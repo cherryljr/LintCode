fd7052470fb75a7a9d2e34054900d2aa343e62ef
cherryljr <travelerljr@gmail.com>
2018-04-07 16:29:49 +0900

    PriorityQueue

--- Merge k Sorted Arrays.java
+++ Merge k Sorted Arrays.java
@@ -1,15 +1,9 @@
-由Merge k sorted list启发。用PriorityQueue,存k个首发element。
-
-PriorityQueue需要存储单位。自己建一个Class Node 存val, x,y index.    
-因为array里没有 'next' pointer，只能存x,y来推next element
-
-```
 /*
+Description
 Given k sorted integer arrays, merge them into one sorted array.
 
 Example
 Given 3 sorted arrays:
-
 [
   [1, 3, 5, 7],
   [2, 4, 6],
@@ -22,20 +16,18 @@ Do it in O(N log k).
 
 N is the total number of integers.
 k is the number of arrays.
-Tags Expand 
-Heap Priority Queue
-*/
 
-/*
-    Since we can't know the node's sibiling, as creating the prirority queue,
-    let's create a Node{val, x, y}
-    Then it's very similar to merge k sorted lists.
-    
-    border case: arrays == null. length == 0 //[]return empty list.
-*/
-    
+Tags
+Heap Priority Queue
+ */
+
+/**
+ * Approach: PriorityQueue
+ * Since we can't know the node's next element, as creating the PriorityQueue minHeap, let's create a Node{val, x, y}
+ * Then it's very similar to merge k sorted lists.
+ */
 public class Solution {
-    public class Node {
+    class Node {
         int val, x, y;
         public Node(int val, int x, int y) {
             this.val = val;
@@ -43,40 +35,34 @@ public class Solution {
             this.y = y;
         }
     }
-    
+
+    /**
+     * @param arrays: k sorted integer arrays
+     * @return: a sorted array
+     */
     public List<Integer> mergekSortedArrays(int[][] arrays) {
-        List<Integer> rst = new ArrayList<Integer>();
+        List<Integer> rst = new ArrayList<>();
         if (arrays == null || arrays.length == 0) {
             return rst;
         }
-        
-        PriorityQueue<Node> heap = new PriorityQueue<Node>(arrays.length,
-            new Comparator<Node>() {
-                public int compare(Node a, Node b){
-                    return a.val - b.val;
-                }
-            }
-        );
-        
-        //init
+
+        PriorityQueue<Node> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+        // Initialize
         for (int i = 0; i < arrays.length; i++) {
+            // Similar to merge k sorted lists, check the array is empty or not
             if (arrays[i].length != 0) {
-                heap.offer(new Node(arrays[i][0], i, 0));
+                minHeap.offer(new Node(arrays[i][0], i, 0));
             }
         }
-        
-        Node node;
-        
-        while (!heap.isEmpty()) {
-            node = heap.poll();
+
+        while (!minHeap.isEmpty()) {
+            Node node = minHeap.poll();
             rst.add(node.val);
             if (node.y < arrays[node.x].length - 1) {
-                heap.offer(new Node(arrays[node.x][node.y + 1], node.x, node.y + 1));
-            }   
+                minHeap.offer(new Node(arrays[node.x][node.y + 1], node.x, node.y + 1));
+            }
         }
-        
-        return rst;
 
+        return rst;
     }
 }
-
