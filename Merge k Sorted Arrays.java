9f58432c0499a7bf201d8aada73d077360b08b88
cherryljr <travelerljr@gmail.com>
2018-04-08 14:03:50 +0900

    PriorityQueue (minHeap)

--- Merge k Sorted Arrays.java
+++ Merge k Sorted Arrays.java
@@ -25,6 +25,15 @@ Heap Priority Queue
  * Approach: PriorityQueue
  * Since we can't know the node's next element, as creating the PriorityQueue minHeap, let's create a Node{val, x, y}
  * Then it's very similar to merge k sorted lists.
+ *
+ * 具体做法：
+ * 利用整体数组的 前k小 来初始化 PriorityQueue(minHeap).
+ * 然后依次从 PriorityQueue 中 poll 出当前的最小值，然后 add 到 rst 中。
+ * 并检查当前被 poll 出来的元素所属的排序数组中是否还存在下一个元素（利用 Node 中的位置信息 x,y）
+ * 存在的话就将其 add 到 PriorityQueue 中。一直重复下去，直到 PriorityQueue 为空。
+ * 
+ * 变形题：
+ * https://github.com/cherryljr/LintCode/blob/master/K%20Spaced%20Array%20Sorting.java
  */
 public class Solution {
     class Node {
@@ -40,24 +49,28 @@ public class Solution {
      * @param arrays: k sorted integer arrays
      * @return: a sorted array
      */
-    public List<Integer> mergekSortedArrays(int[][] arrays) {
-        List<Integer> rst = new ArrayList<>();
+    public int[] mergekSortedArrays(int[][] arrays) {
         if (arrays == null || arrays.length == 0) {
-            return rst;
+            return null;
         }
 
         PriorityQueue<Node> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
-        // Initialize
+        // Initialize the PriorityQueue and Calculate the all size of arrays
+        int total = 0;	
         for (int i = 0; i < arrays.length; i++) {
             // Similar to merge k sorted lists, check the array is empty or not
             if (arrays[i].length != 0) {
                 minHeap.offer(new Node(arrays[i][0], i, 0));
             }
+            total += arrays[i].length;
         }
 
+        int[] rst = new int[total];
+        int index = 0;
         while (!minHeap.isEmpty()) {
             Node node = minHeap.poll();
-            rst.add(node.val);
+            rst[index++] = node.val;
+            // if the next element exists, put it into the minHeap
             if (node.y < arrays[node.x].length - 1) {
                 minHeap.offer(new Node(arrays[node.x][node.y + 1], node.x, node.y + 1));
             }
