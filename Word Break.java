f5cabe76b5604bec8fa36751ec73d5ef699ffaba
cherryljr <travelerljr@gmail.com>
2018-06-11 10:06:34 +0800

    Recursion + Memory Search | DP

--- Word Break.java
+++ Word Break.java
@@ -4,7 +4,7 @@ Given a string s and a dictionary of words dict,
 determine if s can be break into a space-separated sequence of one or more dictionary words.
 
 Example
-Given s = "lintcode", dict = ["lint", "code"].
+Given s = "lintcode", dict = ["lint", "code"]. 
 Return true because "lintcode" can be break as "lint code".
  */
 
