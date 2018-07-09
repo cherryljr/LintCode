637d9f60a33738bcf6dd46001acdba08c88aa156
cherryljr <travelerljr@gmail.com>
2018-07-09 09:49:33 +0800

    Recursion | Bit Operation

--- Pow(x,n).java
+++ Pow(x,n).java
@@ -1,34 +1,27 @@
-注意以下4种情况
- 1. n的正负
- 2. n == 0的情况
- 3.	n == 1的情况
- 4. n / 2 为奇偶数的情况
- 
-该题有两种解法，分别为Divide and Conquer 和 对n进行一个while循环。
-二者的算法时间复杂度均为：O(logN)
-并且题目中涉及到大量奇偶判断和除以2的运算，故可以利用位运算对程序进行进一步的优化。
-
 /*
-Pow(x, n)
-
+Description
 Implement pow(x, n).
 
-Have you met this question in a real interview? Yes
+You don't need to care about the precision of your answer,
+it's acceptable if the expected answer and your answer 's difference is smaller than 1e-3.
+
 Example
 Pow(2.1, 3) = 9.261
 Pow(0, 1) = 0
 Pow(1, 0) = 1
-Note
-You don't need to care about the precision of your answer, it's acceptable if the expected answer and your answer 's difference is smaller than 1e-3.
 
 Challenge
 O(logn) time
+ */
 
-Tags Expand 
-Binary Search LinkedIn Divide and Conquer Mathematics Facebook
-*/
-
-//	Versin 1: Recursive
+/**
+ * Approach 1: Recursion
+ * 快速幂的做法，类似二分的思想。
+ *  x^n = x^(n/2) * x^(n/2)
+ * 注意处理：n == Integer.MIN_VALUE 的 case，否则会超时。
+ *
+ * 时间复杂度：O(logn)
+ */
 public class Solution {
     /**
      * @param x the base number
@@ -36,19 +29,21 @@ public class Solution {
      * @return the result
      */
     public double myPow(double x, int n) {
-        // Write your code here
+        if (n == Integer.MIN_VALUE) {
+            return 0.0;
+        }
         if (n < 0) {
             x = 1 / x;
             n = -n;
         }
-        
+
         if (n == 0) {
             return 1;
         }
         if (n == 1) {
             return x;
         }
-        
+
         if ((n % 2) != 0) {
             return myPow(x * x, n / 2) * x;
         } else {
@@ -57,7 +52,13 @@ public class Solution {
     }
 }
 
-//	Version 2： No Recursive ( While Loop )
+/**
+ * Approach 2: Bit Operation
+ *  1.全面考察指数的正负、底数是否为零等情况。
+ *  2.写出指数的二进制表达，例如13表达为二进制1101。
+ *  3.举例:10^1101 = 10^0001*10^0100*10^1000。
+ *  4.通过 &1 和 >>1 来逐位读取1101，为1时将该位代表的乘数累乘到最终结果。
+ */
 public class Solution {
     /**
      * @param x the base number
@@ -65,50 +66,23 @@ public class Solution {
      * @return the result
      */
     public double myPow(double x, int n) {
-        // Write your code here
+        if (n == Integer.MIN_VALUE) {
+            return 0.0;
+        }
         if (n < 0) {
             x = 1 / x;
             n = -n;
         }
 
-        double ans = 1, tmp = x;
-
+        double rst = 1;
         while (n != 0) {
-            if (n % 2 == 1) {
-                ans *= tmp;
+            if ((n & 1) == 1) {
+                rst *= x;
             }
-            tmp *= tmp;
-            n /= 2;
-        }
-        return ans;
-    }
-}
-
-
-//	Bit Operation
-public class Solution {
-    /**
-     * @param x the base number
-     * @param n the power number
-     * @return the result
-     */
-    public double myPow(double x, int n) {
-        // Write your code here
-        if (n < 0) {
-            x = 1 / x;
-            n = -n;
+            x *= x;
+            n >>= 1;
         }
 
-        double ans = 1, tmp = x;
-
-        while (n != 0) {
-            if ((n & 1) == 1) {      //change
-                ans *= tmp;
-            }
-            tmp *= tmp;
-            n >>= 1;                 //change
-        }
-
-        return ans;
+        return rst;
     }
 }
