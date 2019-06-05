3d5e43b2645163e57f07702fac0d3e1e3ded34a0
cherryljr <TravelerLjr@gmail.com>
2019-06-05 15:15:17 +0800

    DP (Nim Game)

--- Coins in a Line III.java
+++ Coins in a Line III.java
@@ -1,7 +1,7 @@
 /*
 Description
-There are n coins in a line. 
-Two players take turns to take a coin from one of the ends of the line until there are no more coins left. 
+There are n coins in a line.
+Two players take turns to take a coin from one of the ends of the line until there are no more coins left.
 The player with the larger amount of money wins.
 Could you please decide the first player will win or lose?
 
@@ -10,16 +10,16 @@ Given array A = [3,2,2], return true.
 Given array A = [1,2,4], return true.
 Given array A = [1,20,4], return false.
 
-Challenge 
+Challenge
 Follow Up Question:
 If n is even. Is there any hacky algorithm that can decide whether first player will win or lose in O(1) memory and O(n) time?
 
-Tags 
+Tags
 Array Dynamic Programming Game Theory
 */
 
 /**
- * Approach: DP
+ * Approach 1: DP
  * 在 Coins in a Line II 上面的进一步加大难度。
  * 但是主要解体思路仍然与 II 相同。
  * 仍然要使得每一次取完之后，对手取的都是较差的情况。使得对手拿到的硬币总价值最小。
@@ -27,15 +27,15 @@ Array Dynamic Programming Game Theory
  * 然后对每次操作进行分析，得出 状态转移方程（和 II 几乎一样）
  *
  * 游戏局面 (State):
- * dp[i][j] 剩下 i~j 段的硬币时，可以取得的最大值（i为左端位置，j为右端位置）
+ *  dp[i][j] 剩下 i~j 段的硬币时，可以取得的最大值（i为左端位置，j为右端位置）
  * 玩家决策 (Function):
- * 因为硬币总价值一定，为了保证 先手最大，保证取完后对手能取到的最小即可。
- * (死命想法办法坑对方即可)
- * dp[i][j] = preSum[j + 1] - preSum[i] - Math.min(dp[i+1][j], dp[i][j-1]);
- * 注意：当 i==j 时，我们只能取到 values[i].
+ *  因为硬币总价值一定，为了保证 先手最大，保证取完后对手能取到的最小即可。
+ *  (死命想法办法坑对方即可)
+ *  dp[i][j] = preSum[j + 1] - preSum[i] - Math.min(dp[i+1][j], dp[i][j-1]);
+ *  注意：当 i==j 时，我们只能取到 values[i].
  * 游戏终止 (Initialize)：
- * 当唯一剩下的一枚硬币被取走之后，游戏结束。
- * 故当 i==j 时，dp[i][j] = values[i];
+ *  当唯一剩下的一枚硬币被取走之后，游戏结束。
+ *  故当 i==j 时，dp[i][j] = values[i];
  *
  * 实际的流程就是一个 填矩阵 的过程。
  * 我们发现 dp[i][j] 依赖与 dp[i+1][j] 和 dp[i][j-1] 这两个状态。
@@ -44,6 +44,12 @@ Array Dynamic Programming Game Theory
  * 而初始化的 base 就是这个矩阵的 对角线。
  * 按照 从下到上，从左到右 的顺序进行递推，得到最终结果：
  * 矩阵的 右上角 (dp[0][n-1])
+ *
+ * PS.如果硬币的个数为 偶数 个，那么这就是一个先手必胜的游戏，具体分析可以详见
+ * Stone Game: https://github.com/cherryljr/LeetCode/blob/master/Stone%20Game.java
+ * 
+ * 时间复杂度：O(n * sum)
+ * 空间复杂度：O(n * sum)
  */
 public class Solution {
     /**
@@ -52,6 +58,9 @@ public class Solution {
      */
     public boolean firstWillWin(int[] values) {
         int n = values.length;
+        if ((n & 1) == 0) {
+            return true;
+        }
         int[] preSum = new int[n + 1];
         // 计算前缀和以便后面快速计算出各个区间段之和
         for (int i = 1; i <= n; i++) {
@@ -73,6 +82,36 @@ public class Solution {
             }
         }
 
-        return dp[0][n - 1] > preSum[n] / 2;
+        // 这里利用 乘法 替代 除法，省去考虑 preSum[n] 为奇数的情况
+        // 也是平时 coding 的一个技巧，数值比较时尽量避免使用除法（用乘法替代）
+        return dp[0][n - 1] * 2 > preSum[n];
+    }
+}
+
+/**
+ * Approach 2: DP (Space Optimized)
+ */
+public class Solution {
+    /**
+     * @param values: a vector of integers
+     * @return: a boolean which equals to true if the first player will win
+     */
+    public boolean firstWillWin(int[] values) {
+        int n = values.length;
+        if ((n & 1) == 0) {
+            return true;
+        }
+
+        int[] dp = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (i == j) {
+                    dp[i] = values[i];
+                } else {
+                    dp[j] = Math.max(values[i] - dp[j], values[j] - dp[j - 1]);
+                }
+            }
+        }
+        return dp[n - 1] > 0;
     }
 }
